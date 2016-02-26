8f42a77e45cc00f48cadfb086885b6d3bf8ee717
SpoinkyNL <spoinky.nl@gmail.com>
2016-02-26 20:30:05 +0100

    Redid the main enable-mechanism, should be more robust and user friendly.

--- Artemis/Artemis/ViewModels/Abstract/EffectViewModel.cs
+++ Artemis/Artemis/ViewModels/Abstract/EffectViewModel.cs
@@ -7,6 +7,7 @@ namespace Artemis.ViewModels.Abstract
     public abstract class EffectViewModel : Screen
     {
         private EffectSettings _effectSettings;
+        private bool _showDisabledPopup;
 
         public EffectModel EffectModel { get; set; }
         public MainManager MainManager { get; set; }
@@ -24,8 +25,26 @@ namespace Artemis.ViewModels.Abstract
 
         public bool EffectEnabled => MainManager.EffectManager.ActiveEffect == EffectModel;
 
+        public bool ShowDisabledPopup
+        {
+            get { return _showDisabledPopup; }
+            set
+            {
+                if (value == _showDisabledPopup) return;
+                _showDisabledPopup = value;
+                NotifyOfPropertyChange(() => ShowDisabledPopup);
+            }
+        }
+
         public void ToggleEffect()
         {
+            if (!MainManager.ProgramEnabled)
+            {
+                NotifyOfPropertyChange(() => EffectEnabled);
+                ShowDisabledPopup = true;
+                return;
+            }
+
             if (EffectEnabled)
                 MainManager.EffectManager.ClearEffect();
             else
