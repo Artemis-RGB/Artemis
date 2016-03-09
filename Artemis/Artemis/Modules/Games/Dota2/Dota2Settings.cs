dcd88331f60281e91a17e8f5397cf1a4431d5a3e
Logan Saso <jewsofhazard@gmail.com>
2016-03-08 23:36:36 -0800

    Added much more dota support

--- Artemis/Artemis/Modules/Games/Dota2/Dota2Settings.cs
+++ Artemis/Artemis/Modules/Games/Dota2/Dota2Settings.cs
@@ -18,23 +18,31 @@ namespace Artemis.Modules.Games.Dota2
 
         #region Variables
         public string GameDirectory { get; set; }
+        public bool CanCastAbility { get; set; }
+        public bool ShowHealth { get; set; }
         #endregion
 
 
         public override void Load()
         {
+            ShowHealth = Dota2.Default.ShowHealth;
+            CanCastAbility = Dota2.Default.CanCastAbility;
             Enabled = Dota2.Default.Enabled;
             GameDirectory = Dota2.Default.GameDirectory;
         }
 
         public override void Save()
         {
+            Dota2.Default.ShowHealth = ShowHealth;
+            Dota2.Default.CanCastAbility = CanCastAbility;
             Dota2.Default.Enabled = Enabled;
             Dota2.Default.GameDirectory = GameDirectory;
         }
 
         public override void ToDefault()
         {
+            ShowHealth = true;
+            CanCastAbility = true;
             Enabled = false;
             GameDirectory = string.Empty;
         }
