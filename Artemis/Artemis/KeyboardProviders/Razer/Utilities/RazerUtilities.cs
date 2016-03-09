3d7b5b5810982a0c763e8d152163d969df0f2102
Logan Saso <jewsofhazard@gmail.com>
2016-03-08 19:25:47 -0800

    Removed useless code

--- Artemis/Artemis/KeyboardProviders/Razer/Utilities/RazerUtilities.cs
+++ Artemis/Artemis/KeyboardProviders/Razer/Utilities/RazerUtilities.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using Artemis.Utilities;
 using Corale.Colore.Razer.Keyboard.Effects;
 
 namespace Artemis.KeyboardProviders.Razer.Utilities
@@ -11,45 +12,13 @@ namespace Artemis.KeyboardProviders.Razer.Utilities
         {
             var keyboardGrid = Custom.Create();
             if (b.Width > width || b.Height > height)
-                b = ResizeImage(b, width, height);
+                b = ImageUtilities.ResizeImage(b, width, height);
 
             for (var y = 0; y < b.Height; y++)
                 for (var x = 0; x < b.Width; x++)
                     keyboardGrid[y, x] = b.GetPixel(x, y);
 
             return keyboardGrid;
-        }
-
-        /// <summary>
-        ///     Resize the image to the specified width and height.
-        /// </summary>
-        /// <param name="image">The image to resize.</param>
-        /// <param name="width">The width to resize to.</param>
-        /// <param name="height">The height to resize to.</param>
-        /// <returns>The resized image.</returns>
-        public static Bitmap ResizeImage(Image image, int width, int height)
-        {
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
         }
     }
 }
