a8d93889107f338941a1148ba62d6e887e54d1c1
Logan Saso <jewsofhazard@gmail.com>
2016-03-09 21:03:17 -0800

    Added Mana Health and Day/Night to Dota2 Implementation. CSGO and DOTA will crash with a razer keyboard.

--- Artemis/Artemis/Modules/Games/Dota2/Dota2Settings.cs
+++ Artemis/Artemis/Modules/Games/Dota2/Dota2Settings.cs
@@ -20,6 +20,10 @@ namespace Artemis.Modules.Games.Dota2
         public string GameDirectory { get; set; }
         public bool CanCastAbility { get; set; }
         public bool ShowHealth { get; set; }
+        public bool ShowDayCycle { get; set; }
+        public bool CanCastItem { get; set; }
+        public bool ShowMana { get; set; }
+        public bool ShowDead { get; set; }
         #endregion
 
 
@@ -29,24 +33,39 @@ namespace Artemis.Modules.Games.Dota2
             CanCastAbility = Dota2.Default.CanCastAbility;
             Enabled = Dota2.Default.Enabled;
             GameDirectory = Dota2.Default.GameDirectory;
+            CanCastItem = Dota2.Default.CanCastItem;
+            ShowDayCycle = Dota2.Default.ShowDayCycle;
+            ShowMana = Dota2.Default.ShowMana;
+            ShowDead = Dota2.Default.ShowDead;
         }
 
         public override void Save()
         {
+            Dota2.Default.ShowDayCycle = ShowDayCycle;
             Dota2.Default.ShowHealth = ShowHealth;
             Dota2.Default.CanCastAbility = CanCastAbility;
             Dota2.Default.Enabled = Enabled;
             Dota2.Default.GameDirectory = GameDirectory;
+            Dota2.Default.CanCastItem = CanCastItem;
+            Dota2.Default.ShowMana = ShowMana;
+            Dota2.Default.ShowDead = ShowDead;
 
             Dota2.Default.Save();
         }
 
         public override void ToDefault()
         {
-            ShowHealth = true;
-            CanCastAbility = true;
+           
             Enabled = false;
             GameDirectory = string.Empty;
+
+            ShowHealth = true;
+            CanCastAbility = true;
+            ShowDayCycle = true;
+            CanCastItem = true;
+            ShowMana = true;
+            ShowDead = true;
+
         }
     }
 }
