e701f00fdc9d507aa562faba167aa3ff824fe24b
SpoinkyNL <spoinky.nl@gmail.com>
2016-03-16 21:21:13 +0100

    Added Corsair-yellow color profile.

---
+++ Artemis/Artemis/ViewModels/ProfileEditorViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Media;
+using Artemis.KeyboardProviders;
+using Caliburn.Micro;
+
+namespace Artemis.ViewModels
+{
+    internal class ProfileEditorViewModel : Screen
+    {
+        private readonly KeyboardProvider _keyboard;
+
+        public ProfileEditorViewModel(KeyboardProvider keyboard)
+        {
+            _keyboard = keyboard;
+        }
+
+        private ImageSource GenerateKeyboardImage()
+        {
+            return null;
+        }
+    }
+}
