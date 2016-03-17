8be1dae49c29129bdbba59f64c2b75c5c27f3783
SpoinkyNL <spoinky.nl@gmail.com>
2016-03-17 23:10:49 +0100

    Added layers backend skeleton code

---
+++ Artemis/Artemis/Components/Layer.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using Artemis.Components.Abstract;
+
+namespace Artemis.Components
+{
+    public class Layer : LayerComponent
+    {
+        public override void Draw(Graphics g)
+        {
+            // Read properties and draw accordingly
+        }
+    }
+}
---
+++ Artemis/Artemis/Models/LayerConditionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq.Dynamic;
+
+namespace Artemis.Models
+{
+    public class LayerConditionModel
+    {
+        public string Field { get; set; }
+        public string Value { get; set; }
+        public string Operator { get; set; }
+
+        public bool ConditionMet(object subject)
+        {
+            // Put the subject in a list, allowing Dynamic Linq to be used.
+            var subjectList = new List<object> {subject};
+            var res = subjectList.Where($"s => s.{Field} {Operator} {Value}").Any();
+            return res;
+        }
+    }
+}
