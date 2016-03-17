117cf3c6e57183ad1c984c7d196002433f4252ba
SpoinkyNL <spoinky.nl@gmail.com>
2016-03-17 23:10:49 +0100

    Added layers backend skeleton code

---
+++ Artemis/Artemis/Components/LayerComposite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Artemis.Components.Abstract;
+
+namespace Artemis.Components
+{
+    public class LayerComposite : LayerComponent
+    {
+        public List<LayerComponent> LayerComponents { get; set; }
+
+        public override void Draw(Graphics g)
+        {
+            foreach (var layerComponent in LayerComponents)
+                layerComponent.Draw(g);
+        }
+    }
+}
