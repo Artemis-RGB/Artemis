143470103450e175a1762cefacae4b670fe9e5fa
SpoinkyNL <spoinky.nl@gmail.com>
2016-03-20 19:21:22 +0100

    Some tiny changes, only commiting as a backup

--- Artemis/Artemis/ViewModels/ProfileEditorViewModel.cs
+++ Artemis/Artemis/ViewModels/ProfileEditorViewModel.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Media;
-using Artemis.Components;
+using Artemis.DAL;
 using Artemis.KeyboardProviders;
+using Artemis.Models;
 using Caliburn.Micro;
 
 namespace Artemis.ViewModels
 {
     public class ProfileEditorViewModel : Screen
     {
+        private readonly GameModel _gameModel;
         private readonly KeyboardProvider _keyboard;
+        private List<ProfileModel> _profiles;
 
-        public ProfileEditorViewModel(KeyboardProvider keyboard)
+        public ProfileEditorViewModel(GameModel gameModel, KeyboardProvider keyboard)
         {
+            _gameModel = gameModel;
             _keyboard = keyboard;
+
+            GetProfiles();
+        }
+
+        public void GetProfiles()
+        {
+            _profiles = ProfileProvider.GetAll(_gameModel);
         }
 
         private ImageSource GenerateKeyboardImage()
