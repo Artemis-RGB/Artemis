8c92ed5d7fbeae0a446d72700b08835286c29584
SpoinkyNL <spoinky.nl@gmail.com>
2016-03-20 01:28:19 +0100

    Added skeleton code for profiles

---
+++ Artemis/Artemis/DAL/ProfileProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Artemis.Models;
+
+namespace Artemis.DAL
+{
+    internal class ProfileProvider
+    {
+        /// <summary>
+        ///     Get all profiles
+        /// </summary>
+        /// <returns>All profiles</returns>
+        public List<ProfileModel> GetAll()
+        {
+            return new List<ProfileModel>();
+        }
+
+        /// <summary>
+        ///     Get all profiles matching the provided game
+        /// </summary>
+        /// <param name="game">The game to match</param>
+        /// <returns>All profiles matching the provided game</returns>
+        public List<ProfileModel> GetAll(GameModel game)
+        {
+            return GetAll().Where(g => g.GameName.Equals(game.Name)).ToList();
+        }
+
+        /// <summary>
+        ///     Adds or update the given profile.
+        ///     Updates occur when a profile with the same name and game exist.
+        /// </summary>
+        /// <param name="profile">The profile to add or update</param>
+        public void AddOrUpdate(ProfileModel profile)
+        {
+        }
+    }
+}
---
+++ Artemis/Artemis/Models/ProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using Artemis.Components;
+
+namespace Artemis.Models
+{
+    public class ProfileModel
+    {
+        public string Name { get; set; }
+
+        public string KeyboardName { get; set; }
+        public string GameName { get; set; }
+
+        public LayerComposite Layers { get; set; }
+    }
+}
