2c748ca5d1b56195a59d632ce1b7327ecc163a44
SpoinkyNL <spoinky.nl@gmail.com>
2016-06-02 00:45:28 +0200

    Enabled pulse on profile editor glow effect and fixed slide animations

--- Artemis/Artemis/Utilities/Layers/AnimationUpdater.cs
+++ Artemis/Artemis/Utilities/Layers/AnimationUpdater.cs
@@ -17,11 +17,13 @@ namespace Artemis.Utilities.Layers
                 case LayerAnimation.SlideLeft:
                     if (progress + 1 >= animateProperties.Width*scale)
                         progress = 0;
+                    progress = progress + animateProperties.AnimationSpeed * 2;
                     break;
                 case LayerAnimation.SlideDown:
                 case LayerAnimation.SlideUp:
                     if (progress + 1 >= animateProperties.Height*scale)
                         progress = 0;
+                    progress = progress + animateProperties.AnimationSpeed * 2;
                     break;
                 case LayerAnimation.Pulse:
                     if (progress > 2)
