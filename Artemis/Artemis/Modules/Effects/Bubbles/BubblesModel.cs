fe6788d9ef0345b5531809c7c18f98528cfae855
SpoinkyNL <spoinky.nl@gmail.com>
2016-06-13 11:38:46 +0200

    Replaced DynamicLinq with DynamicExpresso

--- Artemis/Artemis/Modules/Effects/Bubbles/BubblesModel.cs
+++ Artemis/Artemis/Modules/Effects/Bubbles/BubblesModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq.Dynamic;
+using System.Linq;
 using System.Windows;
 using Artemis.Managers;
 using Artemis.Models;
