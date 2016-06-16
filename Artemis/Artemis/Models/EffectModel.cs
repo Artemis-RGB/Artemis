eb792cc05f38e63320d342b5428be956c64d86ec
SpoinkyNL <spoinky.nl@gmail.com>
2016-06-16 12:05:34 +0200

    Expanded Trace logging for Overwatch and RL, updated The Witcher 3 mod for 1.22

--- Artemis/Artemis/Models/EffectModel.cs
+++ Artemis/Artemis/Models/EffectModel.cs
@@ -6,7 +6,6 @@ using Artemis.Managers;
 using Artemis.Models.Interfaces;
 using Artemis.Models.Profiles;
 using Newtonsoft.Json;
-using NLog;
 using Brush = System.Windows.Media.Brush;
 
 namespace Artemis.Models
@@ -15,18 +14,18 @@ namespace Artemis.Models
     {
         public delegate void SettingsUpdateHandler(EffectSettings settings);
 
-        public bool Initialized { get; set; }
-        public MainManager MainManager { get; set; }
-        public string Name { get; set; }
-        public int KeyboardScale { get; set; } = 4;
-
-        private DateTime _lastTrace;
+        protected DateTime LastTrace;
 
         protected EffectModel(MainManager mainManager, IDataModel dataModel)
         {
             MainManager = mainManager;
             DataModel = dataModel;
         }
+
+        public bool Initialized { get; set; }
+        public MainManager MainManager { get; set; }
+        public string Name { get; set; }
+        public int KeyboardScale { get; set; } = 4;
 
         // Used by profile system
         public IDataModel DataModel { get; set; }
@@ -41,7 +40,8 @@ namespace Artemis.Models
         public abstract void Update();
 
         // Called after every update
-        public virtual void Render(Graphics keyboard, out Brush mouse, out Brush headset, bool renderMice, bool renderHeadsets)
+        public virtual void Render(Graphics keyboard, out Brush mouse, out Brush headset, bool renderMice,
+            bool renderHeadsets)
         {
             mouse = null;
             headset = null;
@@ -52,24 +52,23 @@ namespace Artemis.Models
             // Get all enabled layers who's conditions are met
             var renderLayers = GetRenderLayers(renderMice, renderHeadsets);
 
-            // Trace debugging
-            if (DateTime.Now.AddSeconds(-2) > _lastTrace)
-            {
-                _lastTrace = DateTime.Now;
-                MainManager.Logger.Trace("Effect datamodel as JSON: \r\n{0}",
-                    JsonConvert.SerializeObject(DataModel, Formatting.Indented));
-                MainManager.Logger.Trace("Effect {0} has to render {1} layers", Name, renderLayers.Count);
-                foreach (var renderLayer in renderLayers)
-                    MainManager.Logger.Trace("    Layer name: {0}, layer type: {1}", renderLayer.Name,
-                        renderLayer.LayerType);
-            }
-
             // Render the keyboard layer-by-layer
-            Profile.DrawProfile(keyboard, renderLayers, DataModel, MainManager.DeviceManager.ActiveKeyboard.KeyboardRectangle(KeyboardScale), false, true);
+            Profile.DrawProfile(keyboard, renderLayers, DataModel,
+                MainManager.DeviceManager.ActiveKeyboard.KeyboardRectangle(KeyboardScale), false, true);
             // Render the first enabled mouse (will default to null if renderMice was false)
             mouse = Profile.GenerateBrush(renderLayers.LastOrDefault(l => l.LayerType == LayerType.Mouse), DataModel);
             // Render the first enabled headset (will default to null if renderHeadsets was false)
             headset = Profile.GenerateBrush(renderLayers.LastOrDefault(l => l.LayerType == LayerType.Headset), DataModel);
+
+            // Trace debugging
+            if (DateTime.Now.AddSeconds(-2) <= LastTrace)
+                return;
+            LastTrace = DateTime.Now;
+            MainManager.Logger.Trace("Effect datamodel as JSON: \r\n{0}",
+                JsonConvert.SerializeObject(DataModel, Formatting.Indented));
+            MainManager.Logger.Trace("Effect {0} has to render {1} layers", Name, renderLayers.Count);
+            foreach (var renderLayer in renderLayers)
+                MainManager.Logger.Trace("- Layer name: {0}, layer type: {1}", renderLayer.Name, renderLayer.LayerType);
         }
 
         public abstract List<LayerModel> GetRenderLayers(bool renderMice, bool renderHeadsets);
