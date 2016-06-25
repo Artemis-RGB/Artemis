a408cc985fe587e0c5e979ea3bc58bba10f8af77
SpoinkyNL <spoinky.nl@gmail.com>
2016-06-25 14:49:23 +0200

    Expanded Overwatch data model (todo: sticky values to avoid false-positives)

--- Artemis/Artemis/Modules/Games/Overwatch/OverwatchDataModel.cs
+++ Artemis/Artemis/Modules/Games/Overwatch/OverwatchDataModel.cs
@@ -10,13 +10,15 @@ namespace Artemis.Modules.Games.Overwatch
         public bool Ability1Ready { get; set; }
         public bool Ability2Ready { get; set; }
         public bool UltimateUsed { get; set; }
+        public bool CanChangeHero { get; set; }
     }
 
     public enum OverwatchStatus
     {
-        Unkown,
+        Unknown,
         InMainMenu,
-        InGame
+        InGame,
+        InCharacterSelect,
     }
 
     public enum OverwatchCharacter
--- Artemis/Artemis/Modules/Games/Overwatch/OverwatchModel.cs
+++ Artemis/Artemis/Modules/Games/Overwatch/OverwatchModel.cs
@@ -9,7 +9,6 @@ using Artemis.Models.Interfaces;
 using Artemis.Models.Profiles;
 using Artemis.Utilities.DataReaders;
 using Caliburn.Micro;
-using Newtonsoft.Json;
 
 namespace Artemis.Modules.Games.Overwatch
 {
@@ -49,27 +48,27 @@ namespace Artemis.Modules.Games.Overwatch
         {
             OverwatchCharacters = new List<CharacterColor>
             {
-                new CharacterColor {Character = OverwatchCharacter.Genji, Color = Color.FromRgb(13, 61, 0)},
-                new CharacterColor {Character = OverwatchCharacter.Mccree, Color = Color.FromRgb(24, 1, 1)},
-                new CharacterColor {Character = OverwatchCharacter.Pharah, Color = Color.FromRgb(0, 6, 32)},
-                new CharacterColor {Character = OverwatchCharacter.Reaper, Color = Color.FromRgb(7, 0, 0)},
-                new CharacterColor {Character = OverwatchCharacter.Soldier76, Color = Color.FromRgb(3, 5, 11)},
-                new CharacterColor {Character = OverwatchCharacter.Tracer, Color = Color.FromRgb(46, 12, 0)},
-                new CharacterColor {Character = OverwatchCharacter.Bastion, Color = Color.FromRgb(6, 10, 5)},
-                new CharacterColor {Character = OverwatchCharacter.Hanzo, Color = Color.FromRgb(28, 24, 8)},
-                new CharacterColor {Character = OverwatchCharacter.Junkrat, Color = Color.FromRgb(59, 28, 0)},
-                new CharacterColor {Character = OverwatchCharacter.Mei, Color = Color.FromRgb(3, 20, 55)},
-                new CharacterColor {Character = OverwatchCharacter.Torbjörn, Color = Color.FromRgb(31, 4, 3)},
-                new CharacterColor {Character = OverwatchCharacter.Widowmaker, Color = Color.FromRgb(16, 3, 17)},
-                new CharacterColor {Character = OverwatchCharacter.Dva, Color = Color.FromRgb(62, 12, 32)},
-                new CharacterColor {Character = OverwatchCharacter.Reinhardt, Color = Color.FromRgb(12, 16, 16)},
-                new CharacterColor {Character = OverwatchCharacter.Roadhog, Color = Color.FromRgb(26, 10, 0)},
-                new CharacterColor {Character = OverwatchCharacter.Winston, Color = Color.FromRgb(17, 18, 26)},
-                new CharacterColor {Character = OverwatchCharacter.Zarya, Color = Color.FromRgb(58, 7, 24)},
-                new CharacterColor {Character = OverwatchCharacter.Lúcio, Color = Color.FromRgb(8, 35, 0)},
-                new CharacterColor {Character = OverwatchCharacter.Mercy, Color = Color.FromRgb(60, 56, 26)},
-                new CharacterColor {Character = OverwatchCharacter.Symmetra, Color = Color.FromRgb(11, 29, 37)},
-                new CharacterColor {Character = OverwatchCharacter.Zenyatta, Color = Color.FromRgb(62, 54, 6)}
+                new CharacterColor {Character = OverwatchCharacter.Genji, Color = Color.FromRgb(55, 245, 0)},
+                new CharacterColor {Character = OverwatchCharacter.Mccree, Color = Color.FromRgb(97, 5, 5)},
+                new CharacterColor {Character = OverwatchCharacter.Pharah, Color = Color.FromRgb(0, 24, 128)},
+                new CharacterColor {Character = OverwatchCharacter.Reaper, Color = Color.FromRgb(28, 0, 2)},
+                new CharacterColor {Character = OverwatchCharacter.Soldier76, Color = Color.FromRgb(14, 21, 45)},
+                new CharacterColor {Character = OverwatchCharacter.Tracer, Color = Color.FromRgb(186, 49, 0)},
+                new CharacterColor {Character = OverwatchCharacter.Bastion, Color = Color.FromRgb(26, 43, 20)},
+                new CharacterColor {Character = OverwatchCharacter.Hanzo, Color = Color.FromRgb(113, 99, 33)},
+                new CharacterColor {Character = OverwatchCharacter.Junkrat, Color = Color.FromRgb(237, 113, 2)},
+                new CharacterColor {Character = OverwatchCharacter.Mei, Color = Color.FromRgb(15, 82, 222)},
+                new CharacterColor {Character = OverwatchCharacter.Torbjörn, Color = Color.FromRgb(125, 18, 12)},
+                new CharacterColor {Character = OverwatchCharacter.Widowmaker, Color = Color.FromRgb(65, 12, 70)},
+                new CharacterColor {Character = OverwatchCharacter.Dva, Color = Color.FromRgb(248, 48, 129)},
+                new CharacterColor {Character = OverwatchCharacter.Reinhardt, Color = Color.FromRgb(51, 65, 66)},
+                new CharacterColor {Character = OverwatchCharacter.Roadhog, Color = Color.FromRgb(107, 40, 2)},
+                new CharacterColor {Character = OverwatchCharacter.Winston, Color = Color.FromRgb(70, 73, 107)},
+                new CharacterColor {Character = OverwatchCharacter.Zarya, Color = Color.FromRgb(235, 28, 97)},
+                new CharacterColor {Character = OverwatchCharacter.Lúcio, Color = Color.FromRgb(34, 142, 2)},
+                new CharacterColor {Character = OverwatchCharacter.Mercy, Color = Color.FromRgb(243, 226, 106)},
+                new CharacterColor {Character = OverwatchCharacter.Symmetra, Color = Color.FromRgb(46, 116, 148)},
+                new CharacterColor {Character = OverwatchCharacter.Zenyatta, Color = Color.FromRgb(248, 218, 26)}
             };
         }
 
@@ -91,8 +90,8 @@ namespace Artemis.Modules.Games.Overwatch
                 return;
 
             _events.PublishOnUIThread(new RazerColorArrayChanged(colors));
-            MainManager.Logger.Trace("DataModel: \r\n{0}",
-                JsonConvert.SerializeObject(gameDataModel, Formatting.Indented));
+            //MainManager.Logger.Trace("DataModel: \r\n{0}",
+            //    JsonConvert.SerializeObject(gameDataModel, Formatting.Indented));
 
             // Determine general game state
             ParseGameSate(gameDataModel, colors);
@@ -101,15 +100,16 @@ namespace Artemis.Modules.Games.Overwatch
             var characterMatch = ParseCharacter(gameDataModel, colors);
 
             // Ult can't possibly be ready within 2 seconds of changing, this avoids false positives.
-            // Filtering on ultready and ultused removes false positives from the native ultimate effects
+            // Filtering on ultReady and ultUsed removes false positives from the native ultimate effects
+            // The control keys don't show during character select, so don't continue on those either.
             if (_characterChange.AddSeconds(2) >= DateTime.Now ||
                 _ultimateUsed.AddSeconds(2) >= DateTime.Now ||
-                _ultimateReady.AddSeconds(2) >= DateTime.Now)
+                _ultimateReady.AddSeconds(2) >= DateTime.Now ||
+                gameDataModel.Status == OverwatchStatus.InCharacterSelect)
                 return;
 
-            ParseUltimate(gameDataModel, characterMatch, colors);
-            ParseAbility1(gameDataModel, colors);
-            ParseAbility2(gameDataModel, colors);
+            ParseSpecialKeys(gameDataModel, characterMatch, colors);
+            ParseAbilities(gameDataModel, colors);
         }
 
         private void ParseGameSate(OverwatchDataModel gameDataModel, Color[,] colors)
@@ -118,7 +118,7 @@ namespace Artemis.Modules.Games.Overwatch
                 return;
             gameDataModel.Status = colors[0, 0].Equals(Color.FromRgb(55, 30, 0))
                 ? OverwatchStatus.InMainMenu
-                : OverwatchStatus.Unkown;
+                : OverwatchStatus.Unknown;
 
             if (gameDataModel.Status != OverwatchStatus.InMainMenu)
                 return;
@@ -132,45 +132,47 @@ namespace Artemis.Modules.Games.Overwatch
 
         private CharacterColor? ParseCharacter(OverwatchDataModel gameDataModel, Color[,] colors)
         {
-            var characterMatch = new CharacterColor {Character = OverwatchCharacter.None};
-
-            // Scan an entire row of keys, minimizing the chance of misreading it due to animations
-            // Could read entire set of arrays but this is cheaper
-            for (var i = 0; i < 22; i++)
-            {
-                characterMatch = OverwatchCharacters.FirstOrDefault(c => c.Color == colors[1, i]);
-                if (characterMatch.Character != OverwatchCharacter.None)
-                    break;
-            }
-
-            if (_ultimateReady.AddSeconds(2) >= DateTime.Now && characterMatch.Character == OverwatchCharacter.None)
-                return null;
-
+            var characterMatch = OverwatchCharacters.FirstOrDefault(c => c.Color == colors[0, 20]);
+            // If a new character was chosen, let the other methods know
             if (characterMatch.Character != gameDataModel.Character)
                 _characterChange = DateTime.Now;
 
+            // If no character was found, this method shouldn't continue
             if (characterMatch.Character == OverwatchCharacter.None)
                 return characterMatch;
 
-            // Only update the character if it's not none
-            // Status will remain on MainMenu/Unknown if this code is not reached
-            gameDataModel.Character = characterMatch.Character;
-            gameDataModel.Status = OverwatchStatus.InGame;
+            // If WASD isn't orange (any of them will do), player is in character select
+            var keyColor = Color.FromRgb(222, 153, 0);
+            if (colors[2, 3] != keyColor && colors[3, 2] != keyColor &&
+                colors[3, 3] != keyColor && colors[3, 4] != keyColor)
+                gameDataModel.Status = OverwatchStatus.InCharacterSelect;
+            else
+                gameDataModel.Status = OverwatchStatus.InGame;
 
+            // Update the datamodel
+            gameDataModel.Character = characterMatch.Character;
             return characterMatch;
         }
 
-        private void ParseUltimate(OverwatchDataModel gameDataModel, CharacterColor? characterMatch, Color[,] colors)
+        private void ParseSpecialKeys(OverwatchDataModel gameDataModel, CharacterColor? characterMatch, Color[,] colors)
         {
-            if (characterMatch == null)
+            if (characterMatch == null || characterMatch.Value.Character == OverwatchCharacter.None)
                 return;
 
             // Ultimate is ready when Q is blinking
-            var ultReady = !characterMatch.Value.Color.Equals(colors[2, 2]);
-            if (!gameDataModel.UltimateReady && ultReady && _ultimateUsed.AddSeconds(15) <= DateTime.Now)
+            var charCol = characterMatch.Value.Color;
+            var backlidColor = Color.FromRgb((byte) (charCol.R*0.25), (byte) (charCol.G*0.25), (byte) (charCol.B*0.25));
+            var ultReady = !backlidColor.Equals(colors[2, 2]);
+
+            if (_ultimateUsed.AddSeconds(15) <= DateTime.Now)
             {
-                _ultimateReady = DateTime.Now;
-                gameDataModel.UltimateReady = true;
+                // Player can change hero if H is blinking
+                gameDataModel.CanChangeHero = !colors[3, 7].Equals(backlidColor);
+                if (!gameDataModel.UltimateReady && ultReady)
+                {
+                    _ultimateReady = DateTime.Now;
+                    gameDataModel.UltimateReady = true;
+                }
             }
 
             // If ult no longer ready but it was ready before, it was used.
@@ -186,13 +188,9 @@ namespace Artemis.Modules.Games.Overwatch
                 gameDataModel.UltimateUsed = _ultimateUsed.AddSeconds(10) >= DateTime.Now;
         }
 
-        private void ParseAbility1(OverwatchDataModel gameDataModel, Color[,] colors)
+        private void ParseAbilities(OverwatchDataModel gameDataModel, Color[,] colors)
         {
             gameDataModel.Ability1Ready = colors[4, 1].Equals(Color.FromRgb(4, 141, 144));
-        }
-
-        private void ParseAbility2(OverwatchDataModel gameDataModel, Color[,] colors)
-        {
             gameDataModel.Ability2Ready = colors[2, 4].Equals(Color.FromRgb(4, 141, 144));
         }
 
