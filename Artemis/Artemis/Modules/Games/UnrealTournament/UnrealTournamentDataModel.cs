0e96d3789fd81c42cc5bfa11653d385199e3495f
SpoinkyNL <spoinky.nl@gmail.com>
2016-08-09 21:27:13 +0200

    UT WIP

---
+++ Artemis/Artemis/Modules/Games/UnrealTournament/UnrealTournamentDataModel.cs
@@ -0,0 +1,34 @@
+﻿using Artemis.Models.Interfaces;
+
+namespace Artemis.Modules.Games.UnrealTournament
+{
+    public class UnrealTournamentDataModel : IDataModel
+    {
+        public State State { get; set; }
+        public Environment Environment { get; set; }
+        public Player Player { get; set; }
+    }
+
+    public enum State
+    {
+        MainMenu,
+        Spectating,
+        Alive,
+        Dead
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public string Powerup { get; set; }
+    }
+
+    public class Environment
+    {
+        public string Mode { get; set; }
+        public string MapName { get; set; }
+    }
+}
---
+++ Artemis/Artemis/Modules/Games/UnrealTournament/UnrealTournamentSettings.cs
@@ -0,0 +1,31 @@
+﻿using Artemis.Models;
+
+namespace Artemis.Modules.Games.UnrealTournament
+{
+    public class UnrealTournamentSettings : GameSettings
+    {
+        public UnrealTournamentSettings()
+        {
+            Load();
+        }
+
+        public sealed override void Load()
+        {
+            Enabled = UnrealTournament.Default.Enabled;
+            LastProfile = UnrealTournament.Default.LastProfile;
+        }
+
+        public sealed override void Save()
+        {
+            UnrealTournament.Default.Enabled = Enabled;
+            UnrealTournament.Default.LastProfile = LastProfile;
+
+            UnrealTournament.Default.Save();
+        }
+
+        public sealed override void ToDefault()
+        {
+            Enabled = true;
+        }
+    }
+}
