9069607fefd7506ae687c2aa1fed91f8bd9f6b42
SpoinkyNL <spoinky.nl@gmail.com>
2016-09-19 19:39:32 +0200

    1.3.4.0 Fixed pulse/grow effects display in editor Fixed crash that could occur on layer deslection Updated Witcher 3 mod for 1.31 Updated almost all profiles to include a mousemat layer Updated all windows profiles to include all device types Added windows profiles to key waves and audio visualization

--- Artemis/Artemis/Properties/AssemblyInfo.cs
+++ Artemis/Artemis/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.3.0")]
-[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyFileVersion("1.3.4.0")]
