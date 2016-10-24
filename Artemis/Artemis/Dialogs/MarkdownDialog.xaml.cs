b799250eb073214d6b85c9a259d646e2560c3c8a
SpoinkyNL <spoinky.nl@gmail.com>
2016-10-24 23:30:10 +0200

    Added markdown support to the changelog display Added live log to the debug window

---
+++ Artemis/Artemis/Dialogs/MarkdownDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Artemis.Dialogs
+{
+    /// <summary>
+    ///     Interaction logic for MarkdownDialog.xaml
+    /// </summary>
+    public partial class MarkdownDialog
+    {
+        public static readonly DependencyProperty MarkdownProperty = DependencyProperty.Register("Markdown",
+            typeof(string), typeof(MarkdownDialog), new PropertyMetadata(default(string)));
+
+        public MarkdownDialog()
+        {
+            InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(NavigationCommands.GoToPage,
+                (sender, e) => System.Diagnostics.Process.Start((string) e.Parameter)));
+        }
+
+        public string Markdown
+        {
+            get { return (string) GetValue(MarkdownProperty); }
+            set { SetValue(MarkdownProperty, value); }
+        }
+    }
+}
