065940c1f9b81f6b504fa92979bb7390ad0ba64f
SpoinkyNL <spoinky.nl@gmail.com>
2016-11-07 14:55:15 +0100

    Fixed Eurotruck Simulator LUA

--- Artemis/Artemis/Modules/Games/EurotruckSimulator2/EurotruckSimulator2DataModel.cs
+++ Artemis/Artemis/Modules/Games/EurotruckSimulator2/EurotruckSimulator2DataModel.cs
@@ -1,23 +1,26 @@
 ﻿using Artemis.Models.Interfaces;
 using Artemis.Modules.Games.EurotruckSimulator2.Data;
 using MoonSharp.Interpreter;
-using MoonSharp.Interpreter.Interop;
 
 namespace Artemis.Modules.Games.EurotruckSimulator2
 {
     [MoonSharpUserData]
     public class EurotruckSimulator2DataModel : IDataModel
     {
-        // TODO: Test LUA functionality
-        [MoonSharpVisible(true)]
+        public EurotruckSimulator2DataModel()
+        {
+            // Register types for LUA
+            UserData.RegisterType<IEts2Game>();
+            UserData.RegisterType<IEts2Job>();
+            UserData.RegisterType<IEts2Navigation>();
+            UserData.RegisterType<IEts2Trailer>();
+            UserData.RegisterType<IEts2Truck>();
+        }
+
         public IEts2Game Game { get; set; }
-        [MoonSharpVisible(true)]
         public IEts2Job Job { get; set; }
-        [MoonSharpVisible(true)]
         public IEts2Navigation Navigation { get; set; }
-        [MoonSharpVisible(true)]
         public IEts2Trailer Trailer { get; set; }
-        [MoonSharpVisible(true)]
         public IEts2Truck Truck { get; set; }
     }
 }
