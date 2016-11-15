bc5e450d783ef7011c7ccbcec1f45611a570c88e
SpoinkyNL <spoinky.nl@gmail.com>
2016-11-16 00:41:24 +0100

    Bump version number

--- Artemis/Artemis/Properties/AssemblyInfo.cs
+++ Artemis/Artemis/Properties/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.1")]
+[assembly: AssemblyFileVersion("1.5.0.1")]
 [assembly: InternalsVisibleTo("Artemis.Tests")]
 [assembly: InternalsVisibleTo("Artemis.Explorables")]
 
