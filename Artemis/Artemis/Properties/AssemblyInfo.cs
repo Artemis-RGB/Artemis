590d6991feac50a7927288b3c9f020d0c752c612
SpoinkyNL <spoinky.nl@gmail.com>
2016-11-20 10:13:59 +0100

    Fixed crash on WebServer start failure for #200 Added Sombra (and Ana) for #201

--- Artemis/Artemis/Properties/AssemblyInfo.cs
+++ Artemis/Artemis/Properties/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.0.2")]
-[assembly: AssemblyFileVersion("1.5.0.2")]
+[assembly: AssemblyVersion("1.5.0.4")]
+[assembly: AssemblyFileVersion("1.5.0.4")]
 [assembly: InternalsVisibleTo("Artemis.Tests")]
 [assembly: InternalsVisibleTo("Artemis.Explorables")]
 
