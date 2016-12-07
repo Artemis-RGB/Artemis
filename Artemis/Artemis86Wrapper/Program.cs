d9d0d8957e04007d15c30a1cbcac67d34afe7ce6
SpoinkyNL <spoinky.nl@gmail.com>
2016-12-07 11:40:02 +0100

    x86 wrapper

---
+++ Artemis/Artemis86Wrapper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Artemis86Wrapper
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Artemis x86 wrapper");
+            var skypeManager = new SkypeManager();
+            skypeManager.Start();
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Artemis/Artemis86Wrapper/SkypeManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+using SKYPE4COMLib;
+
+namespace Artemis86Wrapper
+{
+    public class SkypeManager
+    {
+        private readonly Timer _loopTimer;
+
+        public SkypeManager()
+        {
+            _loopTimer = new Timer(5000);
+            _loopTimer.Elapsed += UpdateSkype;
+        }
+
+        public Skype Skype { get; set; }
+
+        public void Start()
+        {
+            Skype = new Skype();
+            _loopTimer.Start();
+        }
+
+        public void Stop()
+        {
+            _loopTimer.Stop();
+            Skype = null;
+        }
+
+        private void UpdateSkype(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Missed messages: " + Skype.Messages.Count);
+        }
+    }
+}
