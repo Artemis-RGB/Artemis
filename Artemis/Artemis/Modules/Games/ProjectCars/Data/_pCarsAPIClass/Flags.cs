1bbeed48909e83a26052074e06971d7adba85afc
SpoinkyNL <spoinky.nl@gmail.com>
2016-12-18 12:03:49 +0100

    Added Project CARS support

---
+++ Artemis/Artemis/Modules/Games/ProjectCars/Data/_pCarsAPIClass/Flags.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace Artemis.Modules.Games.ProjectCars.Data
+{
+    public partial class pCarsDataClass : INotifyPropertyChanged
+    {
+        private eFlagColors mflagcolour;
+        private eFlagReason mflagreason;
+
+        public eFlagColors mFlagColour
+        {
+            get { return mflagcolour; }
+            set
+            {
+                if (mflagcolour == value)
+                    return;
+                SetProperty(ref mflagcolour, value);
+            }
+        } // [ enum (Type#5) Flag Colour ]
+
+        public eFlagReason mFlagReason
+        {
+            get { return mflagreason; }
+            set
+            {
+                if (mflagreason == value)
+                    return;
+                SetProperty(ref mflagreason, value);
+            }
+        } // [ enum (Type#6) Flag Reason ]
+    }
+}
---
+++ Artemis/Artemis/Modules/Games/ProjectCars/ProjectCarsDataModel.cs
@@ -0,0 +1,17 @@
+﻿using Artemis.Models.Interfaces;
+using Artemis.Modules.Games.ProjectCars.Data;
+using MoonSharp.Interpreter;
+
+namespace Artemis.Modules.Games.ProjectCars
+{
+    [MoonSharpUserData]
+    public class ProjectCarsDataModel : IDataModel
+    {
+        public ProjectCarsDataModel()
+        {
+            GameData = new pCarsDataClass();
+        }
+
+        public pCarsDataClass GameData { get; set; }
+    }
+}
