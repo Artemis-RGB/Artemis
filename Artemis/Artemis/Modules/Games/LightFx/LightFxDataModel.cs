c73c671f01af9f296d08239fca51253095d93ec2
SpoinkyNL <spoinky.nl@gmail.com>
2016-12-21 14:34:40 +0100

    Added LightFX module

---
+++ Artemis/Artemis/Modules/Games/LightFx/LightFxDataModel.cs
@@ -0,0 +1,15 @@
+﻿using Artemis.Models.Interfaces;
+using MoonSharp.Interpreter;
+
+namespace Artemis.Modules.Games.LightFx
+{
+    [MoonSharpUserData]
+    public class LightFxDataModel : IDataModel
+    {
+        public LightFxDataModel()
+        {
+            
+        }
+        
+    }
+}
---
+++ Artemis/Artemis/Modules/Games/LightFx/LightFxViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Artemis.Managers;
+using Artemis.Models;
+using Artemis.ViewModels.Abstract;
+using Ninject;
+
+namespace Artemis.Modules.Games.LightFx
+{
+    public sealed class LightFxViewModel : GameViewModel
+    {
+        public LightFxViewModel(MainManager main, IKernel kernel,
+            [Named("LightFxModel")] GameModel model) : base(main, model, kernel)
+        {
+            DisplayName = "Light FX";
+        }
+    }
+}
