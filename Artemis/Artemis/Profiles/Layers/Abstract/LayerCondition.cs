ef19348e53201fac5fcd9e624d003606965348b0
SpoinkyNL <spoinky.nl@gmail.com>
2017-02-21 22:16:03 +0100

    Reworked key-held keybind

--- Artemis/Artemis/Profiles/Layers/Abstract/LayerCondition.cs
+++ Artemis/Artemis/Profiles/Layers/Abstract/LayerCondition.cs
@@ -1,11 +1,39 @@
 ﻿using Artemis.Modules.Abstract;
 using Artemis.Profiles.Layers.Models;
+using Newtonsoft.Json;
 
 namespace Artemis.Profiles.Layers.Abstract
 {
     public abstract class LayerCondition
     {
+        [JsonIgnore]
+        public bool HotKeyMet { get; set; }
+
         public abstract bool ConditionsMet(LayerModel layerModel, ModuleDataModel dataModel);
-        public abstract void KeybindTask(LayerConditionModel condition);
+
+        
+        public void KeyDownTask(LayerConditionModel condition)
+        {
+            switch (condition.Field)
+            {
+                case "hotkeyEnable":
+                    HotKeyMet = true;
+                    break;
+                case "hotkeyDisable":
+                    HotKeyMet = false;
+                    break;
+                case "hotkeyToggle":
+                    HotKeyMet = !HotKeyMet;
+                    break;
+            }
+        }
+
+        public void KeyUpTask(LayerConditionModel condition)
+        {
+            if (condition.Field == "hotkeyEnable")
+                HotKeyMet = false;
+            else
+                HotKeyMet = true;
+        }
     }
 }
