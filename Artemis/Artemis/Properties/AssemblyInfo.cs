728177fb8704b0bb5fb9451de5ae5f01723da706
Robert Beekman <spoinky.nl@gmail.com>
2017-12-04 10:42:11 +0100

    Fixed logging location
    Bumped version

--- Artemis/Artemis/Properties/AssemblyInfo.cs
+++ Artemis/Artemis/Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.11.0.0")]
-[assembly: AssemblyFileVersion("1.11.0.0")]
+[assembly: AssemblyVersion("1.11.1.0")]
+[assembly: AssemblyFileVersion("1.11.1.0")]
 [assembly: InternalsVisibleTo("Artemis.Explorables")]
