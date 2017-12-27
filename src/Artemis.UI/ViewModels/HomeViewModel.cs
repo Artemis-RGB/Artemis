418b3adea7a8b7b006ca55b3f72c45a5aecb93ae
SpoinkyNL <spoinky.nl@gmail.com>
2017-12-27 16:50:34 +0100

    And the winner is... Stylet!

---
+++ src/Artemis.UI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Artemis.UI.ViewModels.Interfaces;
+using Stylet;
+
+namespace Artemis.UI.ViewModels
+{
+    public class HomeViewModel : Screen, IMainViewModel
+    {
+        public string Title => "Home";
+
+        public void OpenUrl(string url)
+        {
+            // Don't open anything but valid URIs
+            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+                Process.Start(url);
+        }
+    }
+
+    public interface IMainViewModel : IArtemisViewModel
+    {
+    }
+}
---
+++ src/Artemis.UI/ViewModels/RootViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Artemis.UI.ViewModels.Interfaces;
+using Stylet;
+
+namespace Artemis.UI.ViewModels
+{
+    public class RootViewModel : Conductor<IArtemisViewModel>.Collection.OneActive
+    {
+        public RootViewModel(ICollection<IArtemisViewModel> artemisViewModels)
+        {
+            // Add the built-in items
+            Items.AddRange(artemisViewModels);
+            // Activate the home item
+            ActiveItem = artemisViewModels.First(v => v.GetType() == typeof(HomeViewModel));
+        }
+    }
+}
