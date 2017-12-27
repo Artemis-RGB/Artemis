444f3a4280d5b89365c2e3a6c0145ef0a7c90b51
Attila Rakosi <rattila5@hotmail.com>
2017-12-27 11:38:45 -0800

    Made changes to disable Arduino when not in use. Also added some comments.

--- Artemis/Artemis/DeviceProviders/Arduino/ArduinoStrip.cs
+++ Artemis/Artemis/DeviceProviders/Arduino/ArduinoStrip.cs
@@ -18,14 +18,14 @@ namespace Artemis.DeviceProviders.Arduino
 	public class ArduinoStrip : DeviceProvider
 	{
 		private SerialPort _port;
-		const int Width = 288;
+		const int Width = 288;//change this number to number of leds in strip
 		const int Height = 1;
-		private byte[] RGBColors = Enumerable.Repeat((byte)0x20, Width * 3).ToArray();
+		private byte[] RGBColors = Enumerable.Repeat((byte)0x20, Width * 3).ToArray();//initialize the array of colors
 
 		public ArduinoStrip(ILogger logger)
 		{
-			Logger = logger;
-			Type = DeviceType.Generic;
+			Logger = logger;//Hook into the log
+			Type = DeviceType.Generic;//set to Generic type
 		}
 
 		public ILogger Logger { get; set; }
@@ -36,17 +36,24 @@ namespace Artemis.DeviceProviders.Arduino
 				return;
 
 			// Resize the bitmap
-			using (var b = ImageUtilities.ResizeImage(bitmap, Width, Height))
+			using (var b = ImageUtilities.ResizeImage(bitmap, Width, Height))//resize the bitmap to the size we want
 			{
-				for (var x = 0; x < Width; x++)
+				for (var x = 0; x < Width; x++)//loop though all pixels in x direction
 				{
-					var c = b.GetPixel(x, 0);
-					RGBColors[(x * 3) + 0] = c.R;
+					var c = b.GetPixel(x, 0);//get the pixel colors
+					RGBColors[(x * 3) + 0] = c.R;//add color to color array
 					RGBColors[(x * 3) + 1] = c.G;
 					RGBColors[(x * 3) + 2] = c.B;
 				}
 			}
-			_port.Write(RGBColors, 0, Width * 3);
+			try
+			{
+				_port.Write(RGBColors, 0, Width * 3);//try to write to port
+			}
+			catch (Exception)
+			{
+				CanUse = false;//if you cant write to port then disable device
+			}
 		}
 
 		private string AutodetectArduinoPort() //from Brandon https://stackoverflow.com/questions/3293889/how-to-auto-detect-arduino-com-port
@@ -64,26 +71,38 @@ namespace Artemis.DeviceProviders.Arduino
 				{
 					return deviceId;
 				}
-				if (desc.Contains("USB Serial Device"))//my pro micro come up as this
+				if (desc.Contains("USB Serial Device"))//my pro micro come up as "USB Serial Device"
 				{
 					return deviceId;
 				}
 			}
-			return null;
+			return null;//if device not found then return empty
 		}
 
 		public override bool TryEnable()
 		{
 			// Do a check here to see if your device is available. Make sure you set CanUse so
 			// Artemis knows it's available.
-			CanUse = true;
 
-			// Lets open a COM1 connection, this could be an Arduino or something similar
-			if (CanUse)
+			// Lets open COM connection, this could be an Arduino or something similar
+			String port = AutodetectArduinoPort();
+			if (port == null)
 			{
-				_port = new SerialPort(AutodetectArduinoPort(), 9600);
-				_port.WriteBufferSize = Width * 6;
-				_port.Open();
+				CanUse = false;//if no device found then disable
+			}
+			else
+			{
+				CanUse = true;//enable device
+				_port = new SerialPort(port, 115200);//create new port with baud rate of choose like 9600 or 115200
+				_port.WriteBufferSize = Width * 6;//this may not be necessary
+				try
+				{
+					_port.Open();
+				}
+				catch (Exception)
+				{
+					CanUse = false;//if you cant open port then disable device
+				}
 			}
 
 			return CanUse;
--- Artemis/Artemis/DeviceProviders/Arduino/ArduinoStrip.ino
+++ Artemis/Artemis/DeviceProviders/Arduino/ArduinoStrip.ino
@@ -4,18 +4,18 @@ FASTLED_USING_NAMESPACE
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
-#define DATA_PIN    3
-//#define CLK_PIN   4
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define NUM_LEDS    288
+#define DATA_PIN    3 //change to your pin #
+//#define CLK_PIN   4 //some leds need a clock
+#define LED_TYPE    WS2812B //change to your type of led, check link for supported leds https://github.com/FastLED/FastLED/wiki/Chipset-reference
+#define COLOR_ORDER GRB //leds RGB order, WS2812B is GRB
+#define NUM_LEDS    288 //change to you number of leds
 #define SERIAL_BUFFER_SIZE 2048 //this is really huge but it works so...
 CRGB leds[NUM_LEDS];
-#define BRIGHTNESS  20 //0-255  each WS2812B chip draws 0.3W so my 288 strip will draw 86.4Watts thats why I'm limiting to 8% or 6.8W at 5V
+#define BRIGHTNESS  20 //0-255  at 5V each WS2812B chip draws 50mA so my 288 strip will draw 14.4A thats why I'm limiting to 8% or 5.6W
 byte buff[3];
 
 void setup() {
-  Serial.begin(2000000);
+  Serial.begin(2000000);//use buad rate of choose, native USB device will just use max baud rate 
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
   pinMode(LED_BUILTIN_TX, INPUT);//disable TX LED for Arduino pro micro
@@ -27,6 +27,6 @@ void loop() {
   FastLED.show();
   for (int i = 0; i < NUM_LEDS; i++) {
     Serial.readBytes(buff, 3);
-    leds[(i)] = CRGB(buff[1], buff[0], buff[2]);
+    leds[(i)] = CRGB(buff[1], buff[0], buff[2]);//Artemis sends colors as RGB and WS2812B is GRB so I need to change order
   }
 }
