d5db17c0e3c1a59187f7bbec95380e9221528a4e
SpoinkyNL <spoinky.nl@gmail.com>
2019-04-09 19:38:07 +0200

    Added some views, I really just need this code on adifferent machine

---
+++ src/Artemis.UI/ViewModels/Settings/DeviceSettingsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Humanizer;
+using RGB.NET.Core;
+
+namespace Artemis.UI.ViewModels.Settings
+{
+    public class DeviceSettingsViewModel
+    {
+        private readonly IRGBDevice _device;
+
+        public DeviceSettingsViewModel(IRGBDevice device)
+        {
+            _device = device;
+
+            Type = _device.DeviceInfo.DeviceType.ToString().Humanize();
+            Name = _device.DeviceInfo.Model;
+            Manufacturer = _device.DeviceInfo.Manufacturer;
+            Enabled = true;
+        }
+
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
---
+++ src/Artemis.UI/ViewModels/Settings/SettingsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Artemis.Core.Events;
+using Artemis.Core.Services.Interfaces;
+using Artemis.UI.ViewModels.Interfaces;
+using Stylet;
+
+namespace Artemis.UI.ViewModels.Settings
+{
+    public class SettingsViewModel : Screen, ISettingsViewModel
+    {
+        public string Title => "Settings";
+
+        public SettingsViewModel(IRgbService rgbService)
+        {
+            DeviceSettingsViewModels = new List<DeviceSettingsViewModel>();
+            foreach (var device in rgbService.Surface.Devices)
+                DeviceSettingsViewModels.Add(new DeviceSettingsViewModel(device));
+
+            rgbService.DeviceLoaded += UpdateDevices;
+        }
+
+        public List<DeviceSettingsViewModel> DeviceSettingsViewModels { get; set; }
+
+        private void UpdateDevices(object sender, DeviceEventArgs deviceEventArgs)
+        {
+            DeviceSettingsViewModels.Add(new DeviceSettingsViewModel(deviceEventArgs.Device));
+        }
+    }
+}   
