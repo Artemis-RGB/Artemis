51e72cad1607d91580c7fa40bb9c16c73d201d88
Robert Beekman <r.beekman@verentis.com>
2019-08-20 17:30:11 +0200

    Updated Fody
    Add support for overriding ResolvingAbsolutePath

--- src/Artemis.Plugins.Devices.Corsair/CorsairDevice.cs
+++ src/Artemis.Plugins.Devices.Corsair/CorsairDevice.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using Artemis.Core.Plugins.Abstract;
 using Artemis.Core.Plugins.Models;
 using Artemis.Core.Services.Interfaces;
+using RGB.NET.Core;
 using RGB.NET.Devices.Corsair;
 
 namespace Artemis.Plugins.Devices.Corsair
@@ -21,9 +22,19 @@ namespace Artemis.Plugins.Devices.Corsair
 
         public override void EnablePlugin()
         {
+            PathHelper.ResolvingAbsolutePath += ResolveCorsairPath;   
             CorsairDeviceProvider.PossibleX64NativePaths.Add(Path.Combine(PluginInfo.Directory.FullName, "x64", "CUESDK.dll"));
             CorsairDeviceProvider.PossibleX86NativePaths.Add(Path.Combine(PluginInfo.Directory.FullName, "x86", "CUESDK.dll"));
             _rgbService.AddDeviceProvider(CorsairDeviceProvider.Instance);
+        }
+
+        private void ResolveCorsairPath(object sender, ResolvePathEventArgs e)
+        {
+            if (e.RelativePart.Contains("Layouts\\Corsair"))
+            {
+                // Start from the plugin directory
+                e.FinalPath = Path.Combine(PluginInfo.Directory.FullName, e.RelativePart, e.FileName);
+            }
         }
 
         public override void DisablePlugin()
