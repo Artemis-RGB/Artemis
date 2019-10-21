6a07d194e2f686747f34a8734c250f5a6c10e685
Robert <mail@rbeekman.nl>
2019-10-21 13:38:51 +0200

    Moved pan/zoom logic to seperate VM
    Fixed mouse offset when dragging devices and zoomed in
    Update background viewport when zooming/panning

---
+++ src/Artemis.UI/ViewModels/PanZoomViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Stylet;
+
+namespace Artemis.UI.ViewModels
+{
+    public class PanZoomViewModel : PropertyChangedBase
+    {
+        private Point? _lastPanPosition;
+        public double Zoom { get; set; } = 1;
+
+        public double ZoomPercentage
+        {
+            get => Zoom * 100;
+            set => SetZoomFromPercentage(value);
+        }
+
+        public double PanX { get; set; }
+        public double PanY { get; set; }
+
+        public Rect BackgroundViewport => new Rect(PanX, PanY, 20 * Zoom, 20 * Zoom);
+
+        public void ProcessMouseScroll(object sender, MouseWheelEventArgs e)
+        {
+            var relative = GetRelativeMousePosition(sender, e);
+            var absoluteX = relative.X * Zoom + PanX;
+            var absoluteY = relative.Y * Zoom + PanY;
+
+            if (e.Delta > 0)
+                Zoom *= 1.1;
+            else
+                Zoom *= 0.9;
+
+            // Limit to a min of 0.1 and a max of 4 (10% - 400% in the view)
+            Zoom = Math.Max(0.1, Math.Min(4, Zoom));
+
+            // Update the PanX/Y to enable zooming relative to cursor
+            PanX = absoluteX - relative.X * Zoom;
+            PanY = absoluteY - relative.Y * Zoom;
+        }
+
+        public void ProcessMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                Mouse.OverrideCursor = Cursors.Arrow;
+                _lastPanPosition = null;
+                return;
+            }
+
+            if (_lastPanPosition == null)
+                _lastPanPosition = e.GetPosition((IInputElement) sender);
+
+            var position = e.GetPosition((IInputElement) sender);
+            var delta = _lastPanPosition - position;
+            PanX -= delta.Value.X;
+            PanY -= delta.Value.Y;
+
+            _lastPanPosition = position;
+        }
+
+        public void Reset()
+        {
+            Zoom = 1;
+            PanX = 0;
+            PanY = 0;
+        }
+
+        public Rect TransformContainingRect(Rect rect)
+        {
+            // Create the same transform group the view is using
+            var transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new ScaleTransform(Zoom, Zoom));
+            transformGroup.Children.Add(new TranslateTransform(PanX, PanY));
+
+            // Apply it to the device rect
+            return transformGroup.TransformBounds(rect);
+        }
+
+        public Point GetRelativeMousePosition(object container, MouseEventArgs e)
+        {
+            // Get the mouse position relative to the panned / zoomed panel, not very MVVM but I can't find a better way
+            return e.GetPosition(((Panel) container).Children[0]);
+        }
+
+        private void SetZoomFromPercentage(double value)
+        {
+            var newZoom = value / 100;
+            // Focus towards the center of the zoomed area
+            PanX += newZoom - Zoom;
+            PanY += newZoom - Zoom;
+            Zoom = value / 100;
+        }
+    }
+}
