ff959b6f9ceb1b4d2bd61c5368831efaba0e44b0
SpoinkyNL <spoinky.nl@gmail.com>
2019-11-22 23:14:42 +0100

    Disable delete button on active surface
    Resize real-time now that UI FPS is fixed

---
+++ src/Artemis.UI/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Artemis.UI.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
