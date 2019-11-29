c7a5532c3452b663ed79711db81138f536c288f0
SpoinkyNL <spoinky.nl@gmail.com>
2019-11-29 08:47:05 +0100

    Change profile editor VMs to use event aggregator

---
+++ src/Artemis.UI/Events/MainWindowFocusChanged.cs
@@ -0,0 +1,12 @@
+﻿namespace Artemis.UI.Events
+{
+    public class MainWindowFocusChanged
+    {
+        public MainWindowFocusChanged(bool isFocused)
+        {
+            IsFocused = isFocused;
+        }
+
+        public bool IsFocused { get; }
+    }
+}
--- src/Artemis.UI/Screens/Module/ProfileEditor/ProfileEditorPanelViewModel.cs
+++ src/Artemis.UI/Screens/Module/ProfileEditor/ProfileEditorPanelViewModel.cs
@@ -1,22 +1,8 @@
-﻿using Artemis.UI.Screens.Module.ProfileEditor.ProfileElements.ProfileElement;
-using Stylet;
+﻿using Stylet;
 
 namespace Artemis.UI.Screens.Module.ProfileEditor
 {
     public class ProfileEditorPanelViewModel : Screen
     {
-        public ProfileEditorViewModel ProfileEditorViewModel { get; set; }
-
-        public virtual void ActiveProfileChanged()
-        {
-        }
-
-        public virtual void ActiveProfileUpdated()
-        {
-        }
-
-        public virtual void ProfileElementSelected(ProfileElementViewModel profileElement)
-        {
-        }
     }
 }
