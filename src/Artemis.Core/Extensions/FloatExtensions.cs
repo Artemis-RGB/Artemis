2e534eb885dfe0a135e6329b66deb1b551c32084
SpoinkyNL <spoinky.nl@gmail.com>
2019-12-07 16:03:14 +0100

    Optimized noise layer

---
+++ src/Artemis.Core/Extensions/FloatExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Artemis.Core.Extensions
+{
+    public static class FloatExtensions
+    {
+        public static int RoundToInt(this float number)
+        {
+            return (int)Math.Round(number, MidpointRounding.AwayFromZero);
+        }
+    }
+}
