5e876dc8ca9f95aff11f7408344fca198f5c4d15
SpoinkyNL <spoinky.nl@gmail.com>
2019-12-09 00:19:41 +0100

    ColorPicker WIP

---
+++ src/Artemis.UI/Controls/ColorPicker.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Artemis.UI.Controls
+{
+    /// <summary>
+    ///     Interaction logic for ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl, INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+            "Color",
+            typeof(Color),
+            typeof(ColorPicker),
+            new FrameworkPropertyMetadata(default(Color), OnColorPropertyChanged)
+        );
+
+        private byte _colorOpacity;
+
+        private static void OnColorPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is ColorPicker colorPicker)
+            {
+                colorPicker.OnPropertyChanged(nameof(Color));
+                colorPicker.OnPropertyChanged(nameof(ColorCode));
+                colorPicker.OnPropertyChanged(nameof(SolidColor));
+                colorPicker.OnPropertyChanged(nameof(ColorOpacity));
+            }
+        }
+
+        public ColorPicker()
+        {
+            InitializeComponent();
+            PropertyChanged += OnPropertyChanged;
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Color))
+            {
+                if (Color.A != _colorOpacity) 
+                    Color = Color.FromArgb(_colorOpacity, Color.R, Color.G, Color.B);
+            }
+        }
+
+        public Color Color
+        {
+            get => (Color) GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public string ColorCode
+        {
+            get => Color.ToString();
+            set
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                        Color = new Color();
+                    else
+                    {
+                        var color = ColorConverter.ConvertFromString(value);
+                        if (color is Color c)
+                        {
+                            _colorOpacity = c.A;
+                            Color = c;
+                        }
+                    }
+                }
+                catch (FormatException)
+                {
+                    // ignored
+                }
+            }
+        }
+
+        public Color? SolidColor => Color.FromRgb(Color.R, Color.G, Color.B);
+
+        public byte ColorOpacity
+        {
+            get => _colorOpacity;
+            set
+            {
+                _colorOpacity = value;
+                if (Color.A != _colorOpacity)
+                {
+                    Color = Color.FromArgb(_colorOpacity, Color.R, Color.G, Color.B);
+                    OnPropertyChanged(nameof(Color));
+                }
+            }
+        }
+
+        public bool PopupOpen { get; set; }
+
+        private void UIElement_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            PopupOpen = !PopupOpen;
+        }
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
--- src/Artemis.UI/Screens/Workshop/WorkshopViewModel.cs
+++ src/Artemis.UI/Screens/Workshop/WorkshopViewModel.cs
@@ -1,9 +1,38 @@
-﻿using Stylet;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Media;
+using Stylet;
 
 namespace Artemis.UI.Screens.Workshop
 {
     public class WorkshopViewModel : Screen, IScreenViewModel
     {
         public string Title => "Workshop";
+
+        public WorkshopViewModel()
+        {
+            PropertyChanged += OnPropertyChanged;
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Console.WriteLine("Property changed:" + e.PropertyName);
+            Console.WriteLine(TestColor);
+        }
+
+        public Color TestColor { get; set; }
+
+
+        protected override void OnActivate()
+        {
+            TestColor = Color.FromRgb(255, 0, 0);
+            base.OnActivate();
+        }
+
+        protected override void OnDeactivate()
+        {
+            Console.WriteLine(TestColor);
+            base.OnDeactivate();
+        }
     }
 }
