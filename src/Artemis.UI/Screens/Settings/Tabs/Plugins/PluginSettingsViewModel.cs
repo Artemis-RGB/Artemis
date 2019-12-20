62a9c19ae19a9129021aeb9ec596fbae5950e5dd
Robert <mail@rbeekman.nl>
2019-12-20 13:43:19 +0100

    Removed my 'optimisations' from LED samples and made it a lot faster 😓
    Expanded settings

---
+++ src/Artemis.UI/Screens/Settings/Tabs/Plugins/PluginSettingsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Artemis.Core.Plugins.Abstract;
+using Stylet;
+
+namespace Artemis.UI.Screens.Settings.Tabs.Plugins
+{
+    public class PluginSettingsViewModel : PropertyChangedBase
+    {
+        private readonly Plugin _plugin;
+
+        public PluginSettingsViewModel(Plugin plugin)
+        {
+            _plugin = plugin;
+            IsEnabled = true;
+        }
+
+        public string Type => _plugin.GetType().BaseType?.Name ?? _plugin.GetType().Name;
+        public string Name => _plugin.PluginInfo.Name;
+        public string Description => "N.Y.I.";
+        public Version Version => _plugin.PluginInfo.Version;
+        public bool IsEnabled { get; set; }
+    }
+}
