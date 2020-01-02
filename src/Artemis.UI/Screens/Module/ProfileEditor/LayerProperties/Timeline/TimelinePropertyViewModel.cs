e67b44b7418b09433f8038d21a931f0b6307d104
SpoinkyNL <spoinky.nl@gmail.com>
2020-01-02 22:54:50 +0100

    Some WIP stuff on the timeline

---
+++ src/Artemis.UI/Screens/Module/ProfileEditor/LayerProperties/Timeline/TimelinePropertyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stylet;
+
+namespace Artemis.UI.Screens.Module.ProfileEditor.LayerProperties.Timeline
+{
+    public class TimelinePropertyViewModel : PropertyChangedBase
+    {
+    }
+}
