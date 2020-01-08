b734fa2f8fa966d8231e8b93bc1bbde69f77dd53
SpoinkyNL <spoinky.nl@gmail.com>
2020-01-08 21:54:58 +0100

    Added layer properties and keyframes to persistent storage

---
+++ src/Artemis.Storage/Entities/Profile/PropertyEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Artemis.Storage.Entities.Profile
+{
+    public class PropertyEntity
+    {
+        public PropertyEntity()
+        {
+            KeyframeEntities = new List<KeyframeEntity>();
+        }
+
+        public string Id { get; set; }
+        public string ValueType { get; set; }
+        public string Value { get; set; }
+
+        public List<KeyframeEntity> KeyframeEntities { get; set; }
+    }
+
+    public class KeyframeEntity
+    {
+        public TimeSpan Position { get; set; }
+        public string Value { get; set; }
+    }
+}
