18b38658e9f46fd1e635b5f0684c834ce5563109
Robert <mail@rbeekman.nl>
2020-02-13 20:31:54 +0100

    Noise brush - Fixed for new render system

--- src/Artemis.Plugins.LayerBrushes.Noise/NoiseBrush.cs
+++ src/Artemis.Plugins.LayerBrushes.Noise/NoiseBrush.cs
@@ -16,6 +16,7 @@ namespace Artemis.Plugins.LayerBrushes.Noise
         public NoiseBrush(Layer layer, LayerBrushDescriptor descriptor) : base(layer, descriptor)
         {
             MainColorProperty = RegisterLayerProperty<SKColor>("Brush.MainColor", "Main color", "The main color of the noise.");
+            SecondaryColorProperty = RegisterLayerProperty<SKColor>("Brush.SecondaryColor", "Secondary color", "The secondary color of the noise.");
             ScaleProperty = RegisterLayerProperty<SKSize>("Brush.Scale", "Scale", "The scale of the noise.");
             AnimationSpeedProperty = RegisterLayerProperty<float>("Brush.AnimationSpeed", "Animation speed", "The speed at which the noise moves.");
             ScaleProperty.InputAffix = "%";
@@ -25,6 +26,7 @@ namespace Artemis.Plugins.LayerBrushes.Noise
         }
 
         public LayerProperty<SKColor> MainColorProperty { get; set; }
+        public LayerProperty<SKColor> SecondaryColorProperty { get; set; }
         public LayerProperty<SKSize> ScaleProperty { get; set; }
         public LayerProperty<float> AnimationSpeedProperty { get; set; }
 
@@ -41,45 +43,44 @@ namespace Artemis.Plugins.LayerBrushes.Noise
 
         public override void Render(SKCanvas canvas, SKPath path, SKPaint paint)
         {
-            return;
             var mainColor = MainColorProperty.CurrentValue;
-            var scale = ScaleProperty.CurrentValue;
+            var horizontalScale = ScaleProperty.CurrentValue.Width / 400f;
+            var verticalScale = ScaleProperty.CurrentValue.Height / 400f;
 
             // Scale down the render path to avoid computing a value for every pixel
-            var width = (int) (Math.Max(path.Bounds.Width, path.Bounds.Height) / scale.Width);
-            var height = (int) (Math.Max(path.Bounds.Width, path.Bounds.Height) / scale.Height);
+            var width = (int) (path.Bounds.Width * horizontalScale);
+            var height = (int) (path.Bounds.Height * verticalScale);
+
             var opacity = (float) Math.Round(mainColor.Alpha / 255.0, 2, MidpointRounding.AwayFromZero);
             using (var bitmap = new SKBitmap(new SKImageInfo(width, height)))
             {
-                bitmap.Erase(new SKColor(0, 0, 0, 0));
-                // Only compute pixels inside LEDs, due to scaling there may be some rounding issues but it's neglect-able
-                foreach (var artemisLed in Layer.Leds)
+                bitmap.Erase(SKColor.Empty);
+                for (var x = 0; x < width; x++)
                 {
-                    var xStart = artemisLed.AbsoluteRenderRectangle.Left / scale.Width;
-                    var xEnd = artemisLed.AbsoluteRenderRectangle.Right / scale.Width;
-                    var yStart = artemisLed.AbsoluteRenderRectangle.Top / scale.Height;
-                    var yEnd = artemisLed.AbsoluteRenderRectangle.Bottom / scale.Height;
-
-                    for (var x = xStart; x < xEnd; x++)
+                    for (var y = 0; y < height; y++)
                     {
-                        for (var y = yStart; y < yEnd; y++)
-                        {
-                            var v = _noise.Evaluate(scale.Width * x / width, scale.Height * y / height, _z);
-                            var alpha = (byte) ((v + 1) * 127 * opacity);
-                            // There's some fun stuff we can do here, like creating hard lines
-                            // if (alpha > 128)
-                            //     alpha = 255;
-                            // else
-                            //     alpha = 0;
-                            bitmap.SetPixel((int) x, (int) y, new SKColor(mainColor.Red, mainColor.Green, mainColor.Blue, alpha));
-                        }
+                        var v = _noise.Evaluate(1f / horizontalScale * x, 1f / verticalScale * y, _z);
+                        var alpha = (byte) ((v + 1) * 127 * opacity);
+                        // There's some fun stuff we can do here, like creating hard lines
+//                        if (alpha > 128)
+//                            alpha = 255;
+//                        else
+//                            alpha = 0;
+                        bitmap.SetPixel(x, y, new SKColor(mainColor.Red, mainColor.Green, mainColor.Blue, alpha));
                     }
                 }
 
-                using (var sh = SKShader.CreateBitmap(bitmap, SKShaderTileMode.Mirror, SKShaderTileMode.Mirror, SKMatrix.MakeScale(scale.Width, scale.Height)))
+
+                var makeTranslation = SKMatrix.MakeTranslation(path.Bounds.Left, path.Bounds.Top);
+                SKMatrix.Concat(ref makeTranslation, makeTranslation, SKMatrix.MakeScale(1f / horizontalScale, 1f / verticalScale));
+                using (var sh = SKShader.CreateBitmap(bitmap, SKShaderTileMode.Mirror, SKShaderTileMode.Mirror, makeTranslation))
                 {
+                    paint.FilterQuality = SKFilterQuality.Low;
+
+                    paint.Shader = SKShader.CreateColor(SecondaryColorProperty.CurrentValue);
+                    canvas.DrawPath(path, paint);
                     paint.Shader = sh;
-                    canvas.DrawPath(Layer.LayerShape.Path, paint);
+                    canvas.DrawPath(path, paint);
                 }
             }
         }
