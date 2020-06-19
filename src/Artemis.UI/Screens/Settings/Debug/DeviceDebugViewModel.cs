479d9bb4b2f683aa86c40d4ab8649d317d95c95f
SpoinkyNL <mail@rbeekman.nl>
2020-06-19 15:41:02 +0200

    Device debugger - Added placeholder

---
+++ src/Artemis.UI/Screens/Settings/Debug/DeviceDebugViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Artemis.Core.Models.Surface;
+using Stylet;
+
+namespace Artemis.UI.Screens.Settings.Debug
+{
+    public class DeviceDebugViewModel : Screen
+    {
+        public ArtemisDevice Device { get; }
+
+        public DeviceDebugViewModel(ArtemisDevice device)
+        {
+            Device = device;
+        }
+    }
+}
