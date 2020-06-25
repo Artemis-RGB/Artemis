9b09942bda598ca212256d5632f59923feffa018
SpoinkyNL <mail@rbeekman.nl>
2020-06-25 23:24:20 +0200

    UI - Store window size and position

--- src/Artemis.Core/Constants.cs
+++ src/Artemis.Core/Constants.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Artemis.Core.Plugins.Models;
 
 namespace Artemis.Core
 {
     public static class Constants
     {
+        public static readonly string ApplicationFolder = Path.GetDirectoryName(typeof(Constants).Assembly.Location);
         public static readonly string DataFolder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\Artemis\\";
         public static readonly string ConnectionString = $"FileName={DataFolder}\\database.db";
         public static readonly PluginInfo CorePluginInfo = new PluginInfo {Guid = Guid.Parse("ffffffff-ffff-ffff-ffff-ffffffffffff"), Name = "Artemis Core"};
---
+++ src/Artemis.UI/Utilities/WindowState.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using MaterialDesignExtensions.Controls;
+
+namespace Artemis.UI.Utilities
+{
+    public class WindowSize
+    {
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public bool IsMaximized { get; set; }
+
+        public void ApplyFromWindow(MaterialWindow window)
+        {
+            Top = window.Top;
+            Left = window.Left;
+            Height = window.Height;
+            Width = window.Width;
+            IsMaximized = window.WindowState == WindowState.Maximized;
+        }
+
+        public void ApplyToWindow(MaterialWindow window)
+        {
+            window.Top = Top;
+            window.Left = Left;
+            window.Height = Height;
+            window.Width = Width;
+            window.WindowState = IsMaximized ? WindowState.Maximized : WindowState.Normal;
+        }
+    }
+}
