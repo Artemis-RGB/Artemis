a19e754f7c60a9b51b62025a5d3468fc1e974cea
SpoinkyNL <mail@rbeekman.nl>
2020-09-29 21:56:40 +0200

    Dynamic datamodels - Added some required models

---
+++ src/Artemis.Core/Models/Profile/DataModel/DataModelPathPart.cs
@@ -0,0 +1,18 @@
+﻿namespace Artemis.Core
+{
+    /// <summary>
+    ///     Represents a part of a data model path
+    /// </summary>
+    public class DataModelPathPart
+    {
+        /// <summary>
+        ///     Gets the identifier that is associated with this part
+        /// </summary>
+        public string Identifier { get; private set; }
+
+        /// <summary>
+        ///     Gets the type of data model this part of the path points to
+        /// </summary>
+        public DataModelPathPartType Type { get; private set; }
+    }
+}
---
+++ src/Artemis.Core/Models/Profile/DataModel/DataModelPathPartType.cs
@@ -0,0 +1,18 @@
+﻿namespace Artemis.Core
+{
+    /// <summary>
+    ///     Represents a type of data model path
+    /// </summary>
+    public enum DataModelPathPartType
+    {
+        /// <summary>
+        ///     Represents a static data model type that points to a data model defined in code
+        /// </summary>
+        Static,
+
+        /// <summary>
+        ///     Represents a static data model type that points to a data model defined at runtime
+        /// </summary>
+        Dynamic
+    }
+}
