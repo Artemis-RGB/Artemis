ed7383958e6b1fb683d5f2e232e4fc34d4c1139f
SpoinkyNL <mail@rbeekman.nl>
2020-09-29 21:56:40 +0200

    Dynamic datamodels - Added some required models

---
+++ src/Artemis.Core/Models/Profile/DataModel/DataModelPath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Artemis.Core.DataModelExpansions;
+
+namespace Artemis.Core
+{
+    /// <summary>
+    ///     Represents a path that points to a property in data model
+    /// </summary>
+    public class DataModelPath
+    {
+        private readonly LinkedList<DataModelPathPart> _parts;
+
+        internal DataModelPath()
+        {
+            _parts = new LinkedList<DataModelPathPart>();
+        }
+
+        /// <summary>
+        ///     Gets the data model at which this path starts
+        /// </summary>
+        public DataModel DataModel { get; private set; }
+
+        /// <summary>
+        ///     Gets a read-only list of all parts of this path
+        /// </summary>
+        public IReadOnlyCollection<DataModelPathPart> Parts => _parts.ToList().AsReadOnly();
+
+        internal Guid DataModelGuid { get; set; }
+    }
+}
