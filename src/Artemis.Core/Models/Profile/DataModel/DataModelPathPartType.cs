26a60f23cae1c305042fb3a79604d11e2568d6fd
Robert <mail@rbeekman.nl>
2020-10-02 13:06:02 +0200

    Dynamic data models - WIP

--- src/Artemis.Core/Models/Profile/DataModel/DataModelPathPartType.cs
+++ src/Artemis.Core/Models/Profile/DataModel/DataModelPathPartType.cs
@@ -6,6 +6,11 @@
     public enum DataModelPathPartType
     {
         /// <summary>
+        ///     Represents an invalid data model type that points to a missing data model
+        /// </summary>
+        Invalid,
+
+        /// <summary>
         ///     Represents a static data model type that points to a data model defined in code
         /// </summary>
         Static,
