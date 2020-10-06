e0db60b644ec7f7c241826a4b90c91adefbd94ed
SpoinkyNL <mail@rbeekman.nl>
2020-10-06 21:39:03 +0200

    Meta - Enabled nullable ref types in Core project
    Meta - Simplified R# settings to be more in line with defaults, sorry Darth 😝
    Dynamic data models - Implemented add/remove events
    Data model paths - Implemented storage

---
+++ src/Artemis.Core/Events/DynamicDataModelEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Artemis.Core.DataModelExpansions;
+
+namespace Artemis.Core
+{
+    /// <summary>
+    ///     Provides data about dynamic data model related events
+    /// </summary>
+    public class DynamicDataModelEventArgs : EventArgs
+    {
+        internal DynamicDataModelEventArgs(DataModel dynamicDataModel, string key)
+        {
+            DynamicDataModel = dynamicDataModel;
+            Key = key;
+        }
+
+        public DataModel DynamicDataModel { get; }
+        public string Key { get; }
+    }
+}
