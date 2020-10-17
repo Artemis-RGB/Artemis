eff9fa22961f9513bf614422033d5a3419923ecd
Felix Lehmann <felix.lehmann18@gmail.com>
2020-10-17 23:25:19 +0200

    Introduced String Regex Condition Operator
    Require complete string to be a full match
    
    Changed to DarthAffe suggestion
    
    Changed to fit new Condition API and match code style

---
+++ src/Artemis.Core/DefaultTypes/Conditions/Operators/StringMatchesRegexConditionOperator.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Artemis.Core.DefaultTypes {
+    internal class StringMatchesRegexConditionOperator : ConditionOperator<string, string>
+    {
+        public override string Description => "Matches Regex";
+        public override string Icon => "Regex";
+
+        public override bool Evaluate(string text, string regex)
+        {
+            // Ensures full match
+            if (!regex.StartsWith("^"))
+                regex = "^" + regex;
+            if (!regex.EndsWith("$"))
+                regex += "$";
+
+            return Regex.IsMatch(text, regex);
+        }
+    }
+}
