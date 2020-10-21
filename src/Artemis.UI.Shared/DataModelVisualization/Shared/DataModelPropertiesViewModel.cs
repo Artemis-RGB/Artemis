3d753355b4f066396eb16330132c4ca277f8874e
SpoinkyNL <mail@rbeekman.nl>
2020-10-21 12:08:50 +0200

    Data model debugger - Display ToString of object if overridden

--- src/Artemis.UI.Shared/DataModelVisualization/Shared/DataModelPropertiesViewModel.cs
+++ src/Artemis.UI.Shared/DataModelVisualization/Shared/DataModelPropertiesViewModel.cs
@@ -8,20 +8,35 @@ namespace Artemis.UI.Shared
     public class DataModelPropertiesViewModel : DataModelVisualizationViewModel
     {
         private Type _displayValueType;
+        private object _displayValue;
 
         internal DataModelPropertiesViewModel(DataModel dataModel, DataModelVisualizationViewModel parent, DataModelPath dataModelPath) : base(dataModel, parent, dataModelPath)
         {
         }
-        
+
         public Type DisplayValueType
         {
             get => _displayValueType;
             set => SetAndNotify(ref _displayValueType, value);
         }
 
+        public object DisplayValue
+        {
+            get => _displayValue;
+            set => SetAndNotify(ref _displayValue, value);
+        }
+
         public override void Update(IDataModelUIService dataModelUIService)
         {
             DisplayValueType = DataModelPath?.GetPropertyType();
+
+            // Only set a display value if ToString returns useful information and not just the type name
+            object currentValue = GetCurrentValue();
+            if (currentValue != null && currentValue.ToString() != currentValue.GetType().ToString())
+                DisplayValue = currentValue.ToString();
+            else
+                DisplayValue = null;
+
             // Always populate properties
             PopulateProperties(dataModelUIService);
 
@@ -35,6 +50,8 @@ namespace Artemis.UI.Shared
 
         public override object GetCurrentValue()
         {
+            if (Parent == null)
+                return null;
             return Parent.IsRootViewModel ? DataModel : base.GetCurrentValue();
         }
 
