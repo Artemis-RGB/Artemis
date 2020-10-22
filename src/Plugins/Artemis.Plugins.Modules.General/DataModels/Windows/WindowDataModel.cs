f18a7ed14741df98ba8073927926910cbb90c190
Felix Lehmann <felix.lehmann18@gmail.com>
2020-10-22 15:41:59 +0200

    Get colors from active window executable icon

--- src/Plugins/Artemis.Plugins.Modules.General/DataModels/Windows/WindowDataModel.cs
+++ src/Plugins/Artemis.Plugins.Modules.General/DataModels/Windows/WindowDataModel.cs
@@ -1,6 +1,12 @@
 ﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
 using Artemis.Core;
 using Artemis.Core.DataModelExpansions;
+using Artemis.Core.Services;
+using SkiaSharp;
 
 namespace Artemis.Plugins.Modules.General.DataModels.Windows
 {
@@ -14,6 +20,28 @@ namespace Artemis.Plugins.Modules.General.DataModels.Windows
 
             // Accessing MainModule requires admin privileges, this way does not
             ProgramLocation = process.GetProcessFilename();
+
+            // Get Icon colors
+            if(File.Exists(ProgramLocation)) 
+            {
+                MemoryStream mem = new MemoryStream();
+                Icon.ExtractAssociatedIcon(ProgramLocation).Save(mem);
+                mem.Seek(0, SeekOrigin.Begin);
+                var skbm = SKBitmap.Decode(mem);
+                mem.Close();
+
+                IColorQuantizerService colorQuantizer = new ColorQuantizerService();
+                var skClrs = colorQuantizer.Quantize(skbm.Pixels.ToList(), 256).ToList();
+                Colors = new IconColorsDataModel 
+                {
+                    Vibrant = colorQuantizer.FindColorVariation(skClrs, ColorType.Vibrant, true),
+                    LightVibrant = colorQuantizer.FindColorVariation(skClrs, ColorType.LightVibrant, true),
+                    DarkVibrant = colorQuantizer.FindColorVariation(skClrs, ColorType.DarkVibrant, true),
+                    Muted = colorQuantizer.FindColorVariation(skClrs, ColorType.Muted, true),
+                    LightMuted = colorQuantizer.FindColorVariation(skClrs, ColorType.LightMuted, true),
+                    DarkMuted = colorQuantizer.FindColorVariation(skClrs, ColorType.DarkMuted, true),
+                };
+            }
         }
 
         [DataModelIgnore]
@@ -22,5 +50,6 @@ namespace Artemis.Plugins.Modules.General.DataModels.Windows
         public string WindowTitle { get; set; }
         public string ProcessName { get; set; }
         public string ProgramLocation { get; set; }
+        public IconColorsDataModel Colors { get; set; }
     }
 }
