590d30009420e79397e549f673e88693fb02134e
Robert <mail@rbeekman.nl>
2020-10-26 15:59:21 +0100

    Plugins - Removed plugins from the main repo
    Plugin service - Log a warn when no built-in plugins are found instead of crashing

--- src/Plugins/Artemis.Plugins.Modules.General/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e592f239-faa0-4840-9c85-46e5867d06d5")]
