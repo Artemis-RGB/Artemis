564f54c01c08507c3607599c167461f856e613b0
SpoinkyNL <mail@rbeekman.nl>
2020-10-26 21:38:36 +0100

    Plugins - Removed plugins from the main repo

--- src/Plugins/Artemis.Plugins.Devices.Logitech/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("235a45c7-24ad-4f47-b9d4-cd67e610a04d")]
