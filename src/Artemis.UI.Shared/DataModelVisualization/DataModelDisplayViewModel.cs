e2c1303938d20927e32cd3b9f95217809358e2b5
SpoinkyNL <mail@rbeekman.nl>
2020-11-18 23:40:24 +0100

    Shared UI - Added XML comments

--- src/Artemis.UI.Shared/DataModelVisualization/DataModelDisplayViewModel.cs
+++ src/Artemis.UI.Shared/DataModelVisualization/DataModelDisplayViewModel.cs
@@ -3,10 +3,17 @@ using Stylet;
 
 namespace Artemis.UI.Shared
 {
+    /// <summary>
+    ///     Represents a <see cref="DataModel" /> display view model
+    /// </summary>
+    /// <typeparam name="T">The type of the data model</typeparam>
     public abstract class DataModelDisplayViewModel<T> : DataModelDisplayViewModel
     {
         private T _displayValue;
 
+        /// <summary>
+        ///     Gets or sets value that the view model must display
+        /// </summary>
         public T DisplayValue
         {
             get => _displayValue;
@@ -19,11 +26,15 @@ namespace Artemis.UI.Shared
 
         internal override object InternalGuard => null;
 
+        /// <inheritdoc />
         public override void UpdateValue(object model)
         {
             DisplayValue = model is T value ? value : default;
         }
 
+        /// <summary>
+        ///     Occurs when the display value is updated
+        /// </summary>
         protected virtual void OnDisplayValueUpdated()
         {
         }
@@ -50,6 +61,10 @@ namespace Artemis.UI.Shared
         /// </summary>
         internal abstract object InternalGuard { get; }
 
+        /// <summary>
+        ///     Updates the display value
+        /// </summary>
+        /// <param name="model">The value to set</param>
         public abstract void UpdateValue(object model);
     }
 }
