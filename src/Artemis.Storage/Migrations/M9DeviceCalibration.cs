e1151b530ec2b017f028fefc7466b43fc99604b2
Robert <mail@rbeekman.nl>
2020-12-08 16:06:57 +0100

    Device calibration - Storage migration

---
+++ src/Artemis.Storage/Migrations/M9DeviceCalibration.cs
@@ -0,0 +1,27 @@
+﻿using Artemis.Storage.Migrations.Interfaces;
+using LiteDB;
+
+namespace Artemis.Storage.Migrations
+{
+    public class M9DeviceCalibration : IStorageMigration
+    {
+        public int UserVersion => 9;
+
+        /// <inheritdoc />
+        public void Apply(LiteRepository repository)
+        {
+            ILiteCollection<BsonDocument> collection = repository.Database.GetCollection("SurfaceEntity");
+            foreach (BsonDocument bsonDocument in collection.FindAll())
+            {
+                foreach (BsonValue bsonDevice in bsonDocument["DeviceEntities"].AsArray)
+                {
+                    bsonDevice["RedScale"] = 1;
+                    bsonDevice["GreenScale"] = 1;
+                    bsonDevice["BlueScale"] = 1;
+                }
+
+                collection.Update(bsonDocument);
+            }
+        }
+    }
+}
