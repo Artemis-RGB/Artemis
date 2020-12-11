65ca84e4bb41bc95aeab1a0056ba1ed65d08a858
SpoinkyNL <mail@rbeekman.nl>
2020-12-11 22:48:40 +0100

    UI - Added setup wizard

---
+++ src/Artemis.UI/Screens/SetupWizard/SetupWizardViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Artemis.Core;
+using Artemis.Core.Services;
+using Artemis.UI.Screens.SetupWizard.Steps;
+using MaterialDesignExtensions.Controllers;
+using MaterialDesignExtensions.Controls;
+using MaterialDesignExtensions.Model;
+using Stylet;
+
+namespace Artemis.UI.Screens.SetupWizard
+{
+    public class SetupWizardViewModel : Conductor<Screen>.Collection.OneActive
+    {
+        private readonly ISettingsService _settingsService;
+        private StepperController _stepperController;
+
+        public SetupWizardViewModel(ISettingsService settingsService,
+            WelcomeStepViewModel welcome,
+            DevicesStepViewModel devices,
+            LayoutStepViewModel layout,
+            SettingsStepViewModel settings,
+            FinishStepViewModel finish)
+        {
+            _settingsService = settingsService;
+            Items.Add(welcome);
+            Items.Add(devices);
+            Items.Add(layout);
+            Items.Add(settings);
+            Items.Add(finish);
+
+            ActiveItem = Items.First();
+        }
+
+        public void ActiveStepChanged(object sender, ActiveStepChangedEventArgs e)
+        {
+            Stepper stepper = (Stepper) sender;
+            _stepperController = stepper.Controller;
+
+            int activeStepIndex = stepper.Steps.IndexOf(e.Step);
+            ActiveItem = Items[activeStepIndex];
+        }
+
+        public void SkipOrFinishWizard()
+        {
+            RequestClose();
+        }
+
+        protected override void OnClose()
+        {
+            PluginSetting<bool> setting = _settingsService.GetSetting("UI.SetupWizardCompleted", false);
+            setting.Value = true;
+            setting.Save();
+
+            base.OnClose();
+        }
+        
+        public void Continue()
+        {
+            _stepperController.Continue();
+        }
+    }
+}
---
+++ src/Artemis.UI/Screens/SetupWizard/Steps/LayoutStepViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Artemis.Core.Services;
+using Stylet;
+
+namespace Artemis.UI.Screens.SetupWizard.Steps
+{
+    public class LayoutStepViewModel : Screen
+    {
+        private readonly ISurfaceService _surfaceService;
+
+        public LayoutStepViewModel(ISurfaceService surfaceService)
+        {
+            _surfaceService = surfaceService;
+        }
+
+        public void ApplyLeftHandedPreset()
+        {
+            _surfaceService.AutoArrange();
+            SetupWizardViewModel setupWizardViewModel = (SetupWizardViewModel) Parent;
+            setupWizardViewModel.Continue();
+        }
+
+        public void ApplyRightHandedPreset()
+        {
+            _surfaceService.AutoArrange();
+            SetupWizardViewModel setupWizardViewModel = (SetupWizardViewModel) Parent;
+            setupWizardViewModel.Continue();
+        }
+    }
+}
