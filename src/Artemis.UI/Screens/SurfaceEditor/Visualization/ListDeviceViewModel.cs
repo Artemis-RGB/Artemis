ee8e8bc1e0d60a6481a7797452aa64d7c7d17cf2
Robert <mail@rbeekman.nl>
2021-02-20 23:13:28 +0100

    Surface editor - Display temporary color in device list

--- src/Artemis.UI/Screens/SurfaceEditor/Visualization/ListDeviceViewModel.cs
+++ src/Artemis.UI/Screens/SurfaceEditor/Visualization/ListDeviceViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Artemis.Core;
+using SkiaSharp;
 using Stylet;
 
 namespace Artemis.UI.Screens.SurfaceEditor.Visualization
@@ -6,12 +7,19 @@ namespace Artemis.UI.Screens.SurfaceEditor.Visualization
     public class ListDeviceViewModel : PropertyChangedBase
     {
         private bool _isSelected;
+        private SKColor _color;
         public ArtemisDevice Device { get; }
 
         public bool IsSelected
         {
             get => _isSelected;
             set => SetAndNotify(ref _isSelected, value);
+        }
+
+        public SKColor Color
+        {
+            get => _color;
+            set => SetAndNotify(ref _color, value);
         }
 
         public ListDeviceViewModel(ArtemisDevice device)
