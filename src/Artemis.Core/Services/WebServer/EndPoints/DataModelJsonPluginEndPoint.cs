5cf9ba2cb8b15ae89973e2801d1c0a3346b35869
Robert <mail@rbeekman.nl>
2021-03-04 00:26:30 +0100

    Plugin endpoints - Added DataModelJsonPluginEndPoint

---
+++ src/Artemis.Core/Services/WebServer/EndPoints/DataModelJsonPluginEndPoint.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Artemis.Core.DataModelExpansions;
+using Artemis.Core.Modules;
+using EmbedIO;
+using Newtonsoft.Json;
+
+namespace Artemis.Core.Services
+{
+    /// <summary>
+    ///     Represents a plugin web endpoint receiving an object of type <typeparamref name="T" /> and returning any
+    ///     <see cref="object" /> or <see langword="null" />.
+    ///     <para>Note: Both will be deserialized and serialized respectively using JSON.</para>
+    /// </summary>
+    public class DataModelJsonPluginEndPoint<T> : PluginEndPoint where T : DataModel
+    {
+        private readonly Module<T>? _module;
+        private readonly DataModelExpansion<T>? _dataModelExpansion;
+
+        internal DataModelJsonPluginEndPoint(Module<T> module, string name, PluginsModule pluginsModule) : base(module, name, pluginsModule)
+        {
+            _module = module ?? throw new ArgumentNullException(nameof(module));
+
+            ThrowOnFail = true;
+            Accepts = MimeType.Json;
+        }
+
+        internal DataModelJsonPluginEndPoint(DataModelExpansion<T> dataModelExpansion, string name, PluginsModule pluginsModule) : base(dataModelExpansion, name, pluginsModule)
+        {
+            _dataModelExpansion = dataModelExpansion ?? throw new ArgumentNullException(nameof(dataModelExpansion));
+
+            ThrowOnFail = true;
+            Accepts = MimeType.Json;
+        }
+
+        /// <summary>
+        ///     Whether or not the end point should throw an exception if deserializing the received JSON fails.
+        ///     If set to <see langword="false" /> malformed JSON is silently ignored; if set to <see langword="true" /> malformed
+        ///     JSON throws a <see cref="JsonException" />.
+        /// </summary>
+        public bool ThrowOnFail { get; set; }
+
+        #region Overrides of PluginEndPoint
+
+        /// <inheritdoc />
+        protected override async Task ProcessRequest(IHttpContext context)
+        {
+            if (context.Request.HttpVerb != HttpVerbs.Post && context.Request.HttpVerb != HttpVerbs.Put)
+                throw HttpException.MethodNotAllowed("This end point only accepts POST and PUT calls");
+
+            context.Response.ContentType = MimeType.Json;
+
+            using TextReader reader = context.OpenRequestText();
+            try
+            {
+                if (_module != null)
+                    JsonConvert.PopulateObject(await reader.ReadToEndAsync(), _module.DataModel);
+                else
+                    JsonConvert.PopulateObject(await reader.ReadToEndAsync(), _dataModelExpansion!.DataModel);
+            }
+            catch (JsonException)
+            {
+                if (ThrowOnFail)
+                    throw;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Artemis.Core/Services/WebServer/EndPoints/EventArgs/EndpointRequestEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EmbedIO;
+
+namespace Artemis.Core.Services
+{
+    /// <summary>
+    ///     Provides data about endpoint request related events
+    /// </summary>
+    public class EndpointRequestEventArgs : EventArgs
+    {
+        internal EndpointRequestEventArgs(IHttpContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        ///     Gets the HTTP context of the request
+        /// </summary>
+        public IHttpContext Context { get; }
+    }
+}
