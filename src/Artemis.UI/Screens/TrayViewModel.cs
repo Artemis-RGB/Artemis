bf452d5eb6b68a4b28720ecfbdbfdc2f664f88f8
Robert <mail@rbeekman.nl>
2021-03-14 10:35:56 +0100

    Data model debugger - Improve performance on large data models
    Data model debugger - Fixed list item remaining that should be removed
    UI - Added --minimized command line parameter to start Artemis in tray

--- src/Artemis.UI/Screens/TrayViewModel.cs
+++ src/Artemis.UI/Screens/TrayViewModel.cs
@@ -49,9 +49,10 @@ namespace Artemis.UI.Screens
             windowService.ConfigureMainWindowProvider(this);
             messageService.ConfigureNotificationProvider(this);
             bool autoRunning = Bootstrapper.StartupArguments.Contains("--autorun");
+            bool minimized = Bootstrapper.StartupArguments.Contains("--minimized");
             bool showOnAutoRun = settingsService.GetSetting("UI.ShowOnStartup", true).Value;
 
-            if (autoRunning && !showOnAutoRun)
+            if (autoRunning && !showOnAutoRun || minimized)
                 coreService.Initialized += (_, _) => updateService.AutoUpdate();
             else
             {
