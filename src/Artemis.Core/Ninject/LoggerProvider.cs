7000110dcbb6b2d96238d6de8a4e5ee0b773ce5d
Robert <mail@rbeekman.nl>
2021-03-31 19:50:19 +0200

    Logging - Changed file extesion of log files from .txt to .log

--- src/Artemis.Core/Ninject/LoggerProvider.cs
+++ src/Artemis.Core/Ninject/LoggerProvider.cs
@@ -13,7 +13,7 @@ namespace Artemis.Core.Ninject
         private static readonly ILogger Logger = new LoggerConfiguration()
             .Enrich.FromLogContext()
             .Enrich.WithDemystifiedStackTraces()
-            .WriteTo.File(Constants.DataFolder + "logs/Artemis log-.txt",
+            .WriteTo.File(Constants.DataFolder + "logs/Artemis log-.log",
                 rollingInterval: RollingInterval.Day,
                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
             .WriteTo.Debug()
