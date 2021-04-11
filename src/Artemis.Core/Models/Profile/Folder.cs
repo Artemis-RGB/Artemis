b8578a9ccebd2333840b1c7c718bdac936173713
Robert <mail@rbeekman.nl>
2021-04-11 09:34:18 +0200

    Profiles - Better implementation of Enable/Disable while in editor

--- src/Artemis.Core/Models/Profile/Folder.cs
+++ src/Artemis.Core/Models/Profile/Folder.cs
@@ -180,7 +180,7 @@ namespace Artemis.Core
                 throw new ObjectDisposedException("Folder");
 
             // Ensure the folder is ready
-            if (!Enabled || Suspended || Path == null)
+            if (!Enabled || Path == null)
                 return;
 
             // No point rendering if all children are disabled
