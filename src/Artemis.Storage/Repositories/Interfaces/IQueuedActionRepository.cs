a6f52ce4a03f954273f7b2e2930db1d59e005fc6
Robert <mail@rbeekman.nl>
2021-05-25 23:15:40 +0200

    Core - Refactored (unused) queued actions system

---
+++ src/Artemis.Storage/Repositories/Interfaces/IQueuedActionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Artemis.Storage.Entities.General;
+
+namespace Artemis.Storage.Repositories.Interfaces
+{
+    public interface IQueuedActionRepository : IRepository
+    {
+        void Add(QueuedActionEntity queuedActionEntity);
+        void Remove(QueuedActionEntity queuedActionEntity);
+        List<QueuedActionEntity> GetAll();
+        List<QueuedActionEntity> GetByType(string type);
+    }
+}
