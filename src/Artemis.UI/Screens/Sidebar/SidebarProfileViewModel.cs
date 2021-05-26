02db78fc7e51bb37ab803e681570e757e3f90177
Robert <mail@rbeekman.nl>
2021-05-26 20:10:27 +0200

    Sidebar - Tweaks

--- src/Artemis.UI/Screens/Sidebar/SidebarProfileViewModel.cs
+++ src/Artemis.UI/Screens/Sidebar/SidebarProfileViewModel.cs
@@ -5,8 +5,15 @@ namespace Artemis.UI.Screens.Sidebar
 {
     public class SidebarProfileViewModel : Screen
     {
+        private bool _isProfileActive;
         public ProfileDescriptor ProfileDescriptor { get; }
 
+        public bool IsProfileActive
+        {
+            get => _isProfileActive;
+            set => SetAndNotify(ref _isProfileActive, value);
+        }
+        
         public SidebarProfileViewModel(ProfileDescriptor profileDescriptor)
         {
             ProfileDescriptor = profileDescriptor;
