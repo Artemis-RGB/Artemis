d5d3530ca50cf9704a08ee4a67bb4eaa2a2bbf16
Robert <mail@rbeekman.nl>
2022-01-08 17:27:50 +0100

    Meta - Upgrade to .NET 6

--- src/Artemis.Core/Extensions/IEnumerableExtensions.cs
+++ src/Artemis.Core/Extensions/IEnumerableExtensions.cs
@@ -31,69 +31,6 @@ namespace Artemis.Core
     public static class IEnumerableExtensions
     {
         /// <summary>
-        ///     Returns all distinct elements of the given source, where "distinctness"
-        ///     is determined via a projection and the default equality comparer for the projected type.
-        /// </summary>
-        /// <remarks>
-        ///     This operator uses deferred execution and streams the results, although
-        ///     a set of already-seen keys is retained. If a key is seen multiple times,
-        ///     only the first element with that key is returned.
-        /// </remarks>
-        /// <typeparam name="TSource">Type of the source sequence</typeparam>
-        /// <typeparam name="TKey">Type of the projected element</typeparam>
-        /// <param name="source">Source sequence</param>
-        /// <param name="keySelector">Projection for determining "distinctness"</param>
-        /// <returns>
-        ///     A sequence consisting of distinct elements from the source sequence,
-        ///     comparing them by the specified key projection.
-        /// </returns>
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector)
-        {
-            return source.DistinctBy(keySelector, null!);
-        }
-
-        /// <summary>
-        ///     Returns all distinct elements of the given source, where "distinctness"
-        ///     is determined via a projection and the specified comparer for the projected type.
-        /// </summary>
-        /// <remarks>
-        ///     This operator uses deferred execution and streams the results, although
-        ///     a set of already-seen keys is retained. If a key is seen multiple times,
-        ///     only the first element with that key is returned.
-        /// </remarks>
-        /// <typeparam name="TSource">Type of the source sequence</typeparam>
-        /// <typeparam name="TKey">Type of the projected element</typeparam>
-        /// <param name="source">Source sequence</param>
-        /// <param name="keySelector">Projection for determining "distinctness"</param>
-        /// <param name="comparer">
-        ///     The equality comparer to use to determine whether or not keys are equal.
-        ///     If null, the default equality comparer for <c>TSource</c> is used.
-        /// </param>
-        /// <returns>
-        ///     A sequence consisting of distinct elements from the source sequence,
-        ///     comparing them by the specified key projection.
-        /// </returns>
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
-
-            return _();
-
-            IEnumerable<TSource> _()
-            {
-                HashSet<TKey> knownKeys = new(comparer);
-                foreach (TSource element in source)
-                {
-                    if (knownKeys.Add(keySelector(element)))
-                        yield return element;
-                }
-            }
-        }
-
-        /// <summary>
         ///     Returns the index of the provided element inside the read only collection
         /// </summary>
         /// <typeparam name="T">The type of element to find</typeparam>
