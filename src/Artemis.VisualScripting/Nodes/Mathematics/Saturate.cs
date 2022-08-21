36f996eb1a5bc74ea8a665b03fb8f94124e0bc31
Darth Affe <darthaffe@wyrez.org>
2022-08-21 23:51:01 +0200

    Added some nodes

---
+++ src/Artemis.VisualScripting/Nodes/Mathematics/Saturate.cs
@@ -0,0 +1,35 @@
+﻿using Artemis.Core;
+using RGB.NET.Core;
+
+namespace Artemis.VisualScripting.Nodes.Mathematics;
+
+[Node("Saturate", "Clamps the value to be in between 0 and 1", "Mathematics", InputType = typeof(Numeric), OutputType = typeof(Numeric))]
+public class SaturateNode : Node
+{
+    #region Properties & Fields
+
+    public InputPin<Numeric> Value { get; }
+
+    public OutputPin<Numeric> Result { get; }
+
+    #endregion
+
+    #region Constructors
+
+    public SaturateNode()
+        : base("Clamp", "Clamps the value to be in between 0 and 1")
+    {
+        Value = CreateInputPin<Numeric>();
+
+        Result = CreateOutputPin<Numeric>();
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <inheritdoc />
+    public override void Evaluate() => Result.Value = ((float)Value.Value).Clamp(0f, 1f);
+
+    #endregion
+}
