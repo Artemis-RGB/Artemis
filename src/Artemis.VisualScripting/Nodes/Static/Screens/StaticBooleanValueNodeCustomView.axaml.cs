7a48bf788e3806dcebaad6077dce0bf1b89199d5
Robert <mail@rbeekman.nl>
2022-08-21 12:03:46 +0200

    Nodes - Added static boolean node

---
+++ src/Artemis.VisualScripting/Nodes/Static/Screens/StaticBooleanValueNodeCustomView.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace Artemis.VisualScripting.Nodes.Static.Screens;
+
+public partial class StaticBooleanValueNodeCustomView : ReactiveUserControl<StaticBooleanValueNodeCustomViewModel>
+{
+    public StaticBooleanValueNodeCustomView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
