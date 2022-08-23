66b3e35128d7b9ff923407bf32b6d0e5fc4f9ad4
Diogo Trindade <diogotr7@gmail.com>
2022-08-23 14:44:23 +0100

    Nodes - Added Enum Switch node WIP

---
+++ src/Artemis.VisualScripting/Nodes/Branching/EnumBranchModule.cs
@@ -0,0 +1,88 @@
+﻿using Artemis.Core;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemis.VisualScripting.Nodes.Branching
+{
+    [Node("Enum Switch", "Returns a different input based on a switch value", "Operators", InputType = typeof(Enum), OutputType = typeof(object))]
+
+    public class EnumSwitchModule : Node
+    {
+        private readonly Dictionary<Enum, InputPin> _inputPins;
+
+        public OutputPin Output { get; set; }
+
+        public InputPin<Enum> SwitchValue { get; set; }
+
+        public EnumSwitchModule() : base("Enum Branch", "desc")
+        {
+            _inputPins = new();
+
+            Output = CreateOutputPin(typeof(object), "Result");
+            SwitchValue = CreateInputPin<Enum>("Switch");
+
+            SwitchValue.PinConnected += OnSwitchPinConnected;
+            SwitchValue.PinDisconnected += OnSwitchPinDisconnected;
+        }
+
+        public override void Evaluate()
+        {
+            if (_inputPins.TryGetValue(SwitchValue.Value, out var pin) && pin.ConnectedTo.Count != 0)
+            {
+                Output.Value = pin.Value;
+            }
+        }
+
+        private void ChangeType(Type type)
+        {
+            foreach (var input in _inputPins.Values)
+            {
+                input.ChangeType(type);
+            }
+            Output.ChangeType(type);
+        }
+
+        private void OnInputPinDisconnected(object? sender, Core.Events.SingleValueEventArgs<IPin> e)
+        {
+            if (_inputPins.Values.All(i => i.ConnectedTo.Count == 0))
+                ChangeType(typeof(object));
+        }
+
+        private void OnInputPinConnected(object? sender, Core.Events.SingleValueEventArgs<IPin> e)
+        {
+            //change the type of our inputs and output
+            //depending on the first node the user connects to
+            ChangeType(e.Value.Type);
+        }
+
+        private void OnSwitchPinConnected(object? sender, Core.Events.SingleValueEventArgs<IPin> e)
+        {
+            //the user connected an enum to the switch pin.
+            //we need to populate the inputs with all enum options.
+            //the type of the input pins is variable, the same as the output.
+            var switchType = SwitchValue.ConnectedTo.First().Type;
+
+            foreach (var enumValue in Enum.GetValues(switchType).Cast<Enum>())
+            {
+                var pin = CreateInputPin(typeof(object), enumValue.ToString().Humanize(LetterCasing.Sentence));
+                pin.PinConnected += OnInputPinConnected;
+                pin.PinDisconnected += OnInputPinDisconnected;
+                _inputPins.Add(enumValue, pin);
+            }
+        }
+
+        private void OnSwitchPinDisconnected(object? sender, Core.Events.SingleValueEventArgs<IPin> e)
+        {
+            foreach (var input in _inputPins.Values)
+            {
+                RemovePin(input);
+            }
+
+            _inputPins.Clear();
+        }
+    }
+}
