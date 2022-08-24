e54e791062613683dbbfb1420761dd511a74a219
aytac.kayadelen <aytac.kayadelen@obss.com.tr>
2022-08-24 11:15:29 +0300

    added string lLength and IsNullOrEmpty nodes

---
+++ src/Artemis.VisualScripting/Nodes/Text/StringLengthNode.cs
@@ -0,0 +1,24 @@
+﻿using Artemis.Core;
+
+namespace Artemis.VisualScripting.Nodes.Text;
+
+[Node("String Length", "Checks whether the first input is contained in the second input.", 
+    "Text", InputType = typeof(string), OutputType = typeof(Numeric))]
+public class StringLengthNode : Node
+{
+    public StringLengthNode()
+        : base("String Length", "Returns string length.")
+    {
+        Input1 = CreateInputPin<string>();
+        Result = CreateOutputPin<Numeric>();
+    }
+
+    public InputPin<string> Input1 { get; }
+
+    public OutputPin<Numeric> Result { get; }
+
+    public override void Evaluate()
+    {
+        Result.Value = new Numeric((Input1.Value ?? "").Length);
+    }
+}
---
+++ src/Artemis.VisualScripting/Nodes/Text/StrıngNullOrEmpty.cs
@@ -0,0 +1,29 @@
+﻿using Artemis.Core;
+
+namespace Artemis.VisualScripting.Nodes.Text;
+
+[Node("String Null or WhiteSpace", "Checks whether the string is null or white space.", 
+    "Text", InputType = typeof(string), OutputType = typeof(bool))]
+public class StringNullOrWhiteSpaceNode : Node
+{
+    public StringNullOrWhiteSpaceNode()
+        : base("Null or White Space", "Returns true if null or white space")
+    {
+        Input1 = CreateInputPin<string>();
+        TrueResult = CreateOutputPin<bool>("true (Empty)");
+        FalseResult = CreateOutputPin<bool>("false (Not Empty)");
+    }
+
+    public InputPin<string> Input1 { get; }
+
+    public OutputPin<bool> TrueResult { get; }
+
+    public OutputPin<bool> FalseResult { get; }
+
+    public override void Evaluate()
+    {
+        bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(Input1.Value);
+        TrueResult.Value = isNullOrWhiteSpace;
+        FalseResult.Value = !isNullOrWhiteSpace;
+    }
+}
