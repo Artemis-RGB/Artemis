44691dd5a8536ffcf90c6d7953380cac63871cda
Robert <mail@rbeekman.nl>
2023-07-27 20:18:05 +0200

    Show question mark in race condition

--- src/Artemis.UI.Shared/Controls/ProfileConfigurationIcon.axaml.cs
+++ src/Artemis.UI.Shared/Controls/ProfileConfigurationIcon.axaml.cs
@@ -50,11 +50,8 @@ public partial class ProfileConfigurationIcon : UserControl, IDisposable
             {
                 Dispatcher.UIThread.Post(() =>
                 {
-                    if (ConfigurationIcon is null)
-                        return;
-                    
-                    Stream? stream = ConfigurationIcon.GetIconStream();
-                    if (stream == null)
+                    Stream? stream = ConfigurationIcon?.GetIconStream();
+                    if (stream == null || ConfigurationIcon == null)
                         Content = new MaterialIcon {Kind = MaterialIconKind.QuestionMark};
                     else
                         LoadFromBitmap(ConfigurationIcon, stream);
