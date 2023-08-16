4223879e726dfcadaed4188c7dd7d1ee7fde7acc
Diogo Trindade <diogotr7@gmail.com>
2023-08-16 17:27:51 +0100

    UI - Fix layout loading

--- src/Artemis.UI/Screens/Device/Tabs/DeviceLayoutTabView.axaml.cs
+++ src/Artemis.UI/Screens/Device/Tabs/DeviceLayoutTabView.axaml.cs
@@ -16,13 +16,13 @@ public partial class DeviceLayoutTabView : ReactiveUserControl<DeviceLayoutTabVi
 
     private void LayoutPathButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel.DefaultLayoutPath);
+        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel?.DefaultLayoutPath ?? string.Empty);
         ViewModel.ShowCopiedNotification();
     }
 
     private void ImagePathButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel.Device.Layout.Image.LocalPath);
+        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel?.Device?.Layout?.Image?.LocalPath ?? string.Empty);
         ViewModel.ShowCopiedNotification();
     }
 }
