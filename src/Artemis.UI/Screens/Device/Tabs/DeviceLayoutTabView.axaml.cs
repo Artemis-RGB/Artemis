6a02d5031b0209dd7e31573b30f959b6d7085c18
Diogo Trindade <diogotr7@gmail.com>
2023-08-19 19:10:03 +0100

    Early return when copying isn't possible

--- src/Artemis.UI/Screens/Device/Tabs/DeviceLayoutTabView.axaml.cs
+++ src/Artemis.UI/Screens/Device/Tabs/DeviceLayoutTabView.axaml.cs
@@ -16,13 +16,19 @@ public partial class DeviceLayoutTabView : ReactiveUserControl<DeviceLayoutTabVi
 
     private void LayoutPathButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel?.DefaultLayoutPath ?? string.Empty);
+        if (ViewModel?.DefaultLayoutPath is null)
+            return;
+        
+        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel.DefaultLayoutPath);
         ViewModel.ShowCopiedNotification();
     }
 
     private void ImagePathButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel?.Device?.Layout?.Image?.LocalPath ?? string.Empty);
+        if (ViewModel?.Device?.Layout?.Image?.LocalPath is null)
+            return;
+        
+        TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(ViewModel.Device.Layout.Image.LocalPath);
         ViewModel.ShowCopiedNotification();
     }
 }
