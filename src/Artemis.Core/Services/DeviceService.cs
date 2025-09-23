c7fa1c5b6c5c879cc642a7efd898badefb9a8cd6
Robert Obrochta <obrochtarobert13@gmail.com>
2025-09-23 17:51:15 -0500

    Fix to support wake on non-sleep supported devices

--- src/Artemis.Core/Services/DeviceService.cs
+++ src/Artemis.Core/Services/DeviceService.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using Artemis.Core.DeviceProviders;
+using Artemis.Core.Providers;
+using Artemis.Core.Services.Models;
+using Artemis.Storage.Entities.Surface;
+using Artemis.Storage.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using RGB.NET.Core;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Artemis.Core.DeviceProviders;
-using Artemis.Core.Providers;
-using Artemis.Core.Services.Models;
-using Artemis.Storage.Entities.Surface;
-using Artemis.Storage.Repositories.Interfaces;
-using RGB.NET.Core;
-using Serilog;
+using ZstdSharp.Unsafe;
 
 namespace Artemis.Core.Services;
 
@@ -256,8 +258,10 @@ internal class DeviceService : IDeviceService
             try
             {
                 _renderService.Value.IsPaused = true;
-                foreach (DeviceProvider deviceProvider in _pluginManagementService.GetFeaturesOfType<DeviceProvider>().Where(d => d.SuspendSupported))
-                    SuspendDeviceProvider(deviceProvider);
+                foreach (DeviceProvider deviceProvider in _pluginManagementService.GetFeaturesOfType<DeviceProvider>())
+                {
+                    SuspendDeviceProvider(deviceProvider, deviceProvider.SuspendSupported);
+                }
             }
             finally
             {
@@ -278,7 +282,9 @@ internal class DeviceService : IDeviceService
             {
                 _renderService.Value.IsPaused = true;
                 foreach (DeviceProvider deviceProvider in _suspendedDeviceProviders.ToList())
+                {
                     ResumeDeviceProvider(deviceProvider);
+                }
             }
             finally
             {
@@ -287,7 +293,7 @@ internal class DeviceService : IDeviceService
         }
     }
 
-    private void SuspendDeviceProvider(DeviceProvider deviceProvider)
+    private void SuspendDeviceProvider(DeviceProvider deviceProvider, bool callSuspend)
     {
         if (_suspendedDeviceProviders.Contains(deviceProvider))
         {
@@ -298,7 +304,10 @@ internal class DeviceService : IDeviceService
         try
         {
             _pluginManagementService.DisablePluginFeature(deviceProvider, false);
-            deviceProvider.Suspend();
+            if (callSuspend)
+            {
+                deviceProvider.Suspend();
+            }
             _suspendedDeviceProviders.Add(deviceProvider);
             _logger.Information("Device provider {DeviceProvider} suspended", deviceProvider.Info.Name);
         }
